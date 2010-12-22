93851ffda02306c785ba42fd54cce313112c6fee
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2010-12-22 01:12:41 +0000

    - FINALLY, took the time to consolidate and arrange the Serialize/Xmlize functionality.  Clearly documented them so hopefully won't be confusing to someone new to U++ stuff. - Make name of ConfigFile consistent and in the same place for all files, named them more clearly

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -2,11 +2,25 @@
 
 GUI_APP_MAIN
 {
+	Lister lister; // Construct first.  Some serialization/xmilization doesn't take when in constructor. (i.e., grid column widths)
+
+	String binConfigFile = GetHomeDirFile("lister.config.bin");
+	String xmlConfigFile = GetHomeDirFile("lister.config.xml");
 	
-	Lister lister;
-	LoadFromFile(lister, ConfigFile("lister.config.bin")); // Primarily only responsible for Serializing window placement.
+	// Call all class Serialize functions
+	LoadFromFile(lister, binConfigFile); // Primarily only responsible for Serializing window placement.
+	
+	// Call all class Xmlize functions
+	LoadFromXMLFile(lister, xmlConfigFile);
+
 	lister.Run();
-	StoreToFile(lister, ConfigFile("lister.config.bin"));
-	//StoreAsXMLFile(myTool, "app", ConfigFile("data.bin"));
+
+	// Store binary serialized values, usually only placement
+	// Binary serialization files get flushed when any changes are made to their inputs.
+	StoreToFile(lister, binConfigFile);
+
+	// Store text xmlized values.  XML format means user customizations are retained over version releases.  
+	StoreAsXMLFile(lister, NULL, xmlConfigFile);
+
 }
 
