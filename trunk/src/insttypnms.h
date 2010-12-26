e2a61e2575c85807fe225f8c7c7e4b0c8eed657b
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2010-12-26 21:23:27 +0000

    - List of type strings from database. Not sure best way, but I don't like using the pk serial ids since if the database is dumped/recreated, those go bye-bye.

---
+++ trunk/src/insttypnms.h
@@ -0,0 +1,10 @@
+#ifndef _lister_insttypnms_h_
+#define _lister_insttypnms_h_
+
+#define INSTTYPNM_POSTGRESQL       "PostgreSQL"
+#define INSTTYPNM_ORACLE           "Oracle"
+#define INSTTYPNM_MSSQLSERVER      "MS SQL"
+#define INSTTYPNM_FTP              "ftp"
+#define INSTTYPNM_TELNET           "telnet"
+
+#endif
