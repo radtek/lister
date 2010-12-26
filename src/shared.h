e11de614958fc3030a007026eee36ee2b943e020
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2010-12-26 23:02:01 +0000

    - Wrapped PostgreSQLSession so use in lister.h doesn't force hand on control db implementation - Defined UNKNOWN so use of -1 as a new serial id in foreign keys more readable

--- src/shared.h
+++ src/shared.h
@@ -1,13 +1,19 @@
 #ifndef _shared_h_
 #define _shared_h_
 
-#define CAT String().Cat()
-
 #include <CtrlLib/CtrlLib.h>
 #include <GridCtrl/GridCtrl.h>
 #include <DropGrid/DropGrid.h>
 
+#include <PostgreSQL/PostgreSQL.h>
+
 using namespace Upp;
+
+// This must be on this side of using namespace
+typedef PostgreSQLSession ControlSession;
+
+#define CAT String().Cat()
+#define UNKNOWN -1
 
 #define LAYOUTFILE <lister/lister.lay>
 #include <CtrlCore/lay.h>
