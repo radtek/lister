1dec8e0dca659a86d8d5597312f69d40d3066f3b
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2010-12-26 23:40:49 +0000

    - Enhanced ReplaceStr to deal with cases where the replacement string is a member of the sought string. - Checks for infinite recursion. - Detects if sought string is a member of the replacement string and, if so, prevents more than one iteration to prevent infinite expansion. - Added some documentation

--- trunk/src/StringHandler.h
+++ trunk/src/StringHandler.h
@@ -3,23 +3,42 @@
 
 //==========================================================================================	
 String ReplaceStr(const String& s1, const String& find, const String replace) {
-        String string_result;
-        
+	if (find == replace) return s1;
+	bool blockinfiniterecursion = false;
+	
+	// If an expansion replace, prevent running more than once
+	
+	if (replace.Find(find) != -1) {
+		blockinfiniterecursion = true;
+	}
+	
+    String string_result = s1;
+    int found_pos;
+    int find_len = find.GetCount();
+    
+    // Repeat until no occurrences at all, recursively checking (i.e., for cases of double space to single space)
+    
+    while(string_result.Find(find) != -1) {
         int start_pos = 0;
-        int found_pos = 0;
-        int find_len = find.GetCount();
-        int s1_count = s1.GetCount();
+        String startFrom = string_result;
+        string_result.Clear();
         
-        while(((found_pos=s1.Find(find, start_pos))!=-1)){
-                string_result.Cat(s1.Mid(start_pos, found_pos - start_pos));
-                string_result.Cat(replace);
-                start_pos = found_pos + find_len;
-        };
-                
-        if(start_pos<s1.GetCount())
-                string_result.Cat(s1.Mid(start_pos));
-                
-        return (string_result);
+        // Replace all occurrences from left to right, which may create new occurrences next pass
+        
+        while(((found_pos = startFrom.Find(find, start_pos)) != -1)) {
+				// Keep portion between strings found
+				string_result.Cat(startFrom.Mid(start_pos, found_pos - start_pos));
+				string_result.Cat(replace); // add the replacement string
+				start_pos = found_pos + find_len;
+		}
+
+        if(start_pos < startFrom.GetCount())
+        	string_result.Cat(startFrom.Mid(start_pos));
+        
+        if (blockinfiniterecursion) break;
+	}
+
+    return (string_result);
 }
 
 #endif
