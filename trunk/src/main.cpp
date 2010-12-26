80d027bcfe12dfc832a8c7aa601f4590a8b1961f
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2010-12-26 22:39:50 +0000

    - Major changes - Stripped reference to image_shared, iml_source. - Documented reasoning for constructing lister object before running - Documented config differences between binary and XML versions - renamed all config files to be consistent - Built config file paths using information from lister constructor - Put xmlize and serialize calls in main to be all in one place - more documentation!!!!!!

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,26 +1,45 @@
 #include "Lister.h"
 
+#include "image_shared.h"
+#include <Draw/iml_source.h>
+
 GUI_APP_MAIN
 {
-	Lister lister; // Construct first.  Some serialization/xmilization doesn't take when in constructor. (i.e., grid column widths)
+	// Construct first.  Some serialization/xmilization doesn't take affect when in constructor. 
+	// (i.e., grid column widths)
+	
+	Lister lister; 
 
-	String binConfigFile = GetHomeDirFile("lister.config.bin");
-	String xmlConfigFile = GetHomeDirFile("lister.config.xml");
+	// When the dev version is run, it uses the config files in that folder so as to avoid
+	// disturbing the useful user settings on the same computer for the app installed in 
+	// "Program Files".
 	
-	// Call all class Serialize functions
-	LoadFromFile(lister, binConfigFile); // Primarily only responsible for Serializing window placement.
+	String binConfigFile = AppendFileName(lister.configFileFolder, "lister.config.bin");
+	String xmlConfigFile = AppendFileName(lister.configFileFolder, "lister.config.xml");
 	
-	// Call all class Xmlize functions
+	// Call all class Serialize functions.  Each class has either a custom Serialize function
+	// or defaults to base class Ctrl GetData/SetData virtual functions.
+	// Primarily only responsible for Serializing window placement, since U++ SerializePlacement
+	// function handles the problem fairly well.
+	
+	LoadFromFile(lister, binConfigFile); 
+	
+	// Call all class Xmlize functions.  Again these are either custom or from Ctrl base.
+	// Many of the U++ classes never bothered to implement this.
+	
 	LoadFromXMLFile(lister, xmlConfigFile);
 
 	lister.Run();
 
-	// Store binary serialized values, usually only placement
-	// Binary serialization files get flushed when any changes are made to their inputs.
+	// Store any changes user made to window positioning/sizing.
+	// Binary serialization files get flushed when any changes are made to their input
+	// attributes, which is why I only keep window placement here.
+	
 	StoreToFile(lister, binConfigFile);
 
-	// Store text xmlized values.  XML format means user customizations are retained over version releases.  
+	// Store text xmlized values.  XML format means user customizations are retained over 
+	// version releases, which is significant, and sad that U++ does not make this easier.
+	
 	StoreAsXMLFile(lister, NULL, xmlConfigFile);
-
 }
 
