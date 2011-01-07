e1fa1484740ab49f07627b3454c38a2ad2b1a887
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-07 19:19:57 +0000

    - Moved all configuration management to UrpMain in Urp lib.

--- src/lister/main.cpp
+++ src/lister/main.cpp
@@ -1,3 +1,4 @@
+#include <Urp/Urp.h>
 #include "Lister.h"
 
 #include "image_shared.h"
@@ -10,36 +11,6 @@ GUI_APP_MAIN
 	
 	Lister lister; 
 
-	// When the dev version is run, it uses the config files in that folder so as to avoid
-	// disturbing the useful user settings on the same computer for the app installed in 
-	// "Program Files".
-	
-	String binConfigFile = AppendFileName(lister.configFileFolder, "lister.config.bin");
-	String xmlConfigFile = AppendFileName(lister.configFileFolder, "lister.config.xml");
-	
-	// Call all class Serialize functions.  Each class has either a custom Serialize function
-	// or defaults to base class Ctrl GetData/SetData virtual functions.
-	// Primarily only responsible for Serializing window placement, since U++ SerializePlacement
-	// function handles the problem fairly well.
-	
-	LoadFromFile(lister, binConfigFile); 
-	
-	// Call all class Xmlize functions.  Again these are either custom or from Ctrl base.
-	// Many of the U++ classes never bothered to implement this.
-	
-	LoadFromXMLFile(lister, xmlConfigFile);
-
-	lister.Run();
-
-	// Store any changes user made to window positioning/sizing.
-	// Binary serialization files get flushed when any changes are made to their input
-	// attributes, which is why I only keep window placement here.
-	
-	StoreToFile(lister, binConfigFile);
-
-	// Store text xmlized values.  XML format means user customizations are retained over 
-	// version releases, which is significant, and sad that U++ does not make this easier.
-	
-	StoreAsXMLFile(lister, NULL, xmlConfigFile);
+	UrpMain(lister, __FILE__);
 }
 
