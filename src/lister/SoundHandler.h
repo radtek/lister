79142989616caf8b6d35072a42e02cf9d1b23635
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-07 19:38:14 +0000

    - Fixed backslash to cross-compiler forwardslash.  Moved some of the sounds around.  Need to move into source control. - Added enum values for TEST event sounds.

--- src/lister/SoundHandler.h
+++ src/lister/SoundHandler.h
@@ -36,45 +36,42 @@ static WAVEHDR* waveBlocks;
 static volatile int waveFreeBlockCount;
 static int waveCurrentBlock;
 
-#define SOUND_TOAD "C:\\TOAD\\toadload.wav"
-#define SOUND_SWISH "C:\\Documents and Settings\\nbkwdkv\\My Documents\\Visual Studio 2008\\Projects\\ora\\92010NT_CLT\\autorun\\click.wav"
-#define SOUND_DOINK "C:\\blp\\Wintrv\\gecko\\res\\samples\\test.wav"
-#define SOUND_BWZZZ "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\fs_asterisk.wav"
-#define SOUND_TWDLZP "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C801.wav"
-#define SOUND_WCHITA "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C802.wav"
-#define SOUND_WEEWEE "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C804.wav"
-#define SOUND_NYANYANOOW "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C807.wav"
-#define SOUND_BOODOOP "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C810.wav"
-#define SOUND_DDDP "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C813.wav"
-#define SOUND_YADEEDAH "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C825.wav"
-#define SOUND_BZZZZDT "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C826.wav"
-#define SOUND_BRRDEET "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C827.wav"
-#define SOUND_ZEEZEEZEET "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C828.wav"
-#define SOUND_DEEDOP "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\C829.wav"
-#define SOUND_LASER "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Laser SFX.wav"
-#define SOUND_CLINK "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Clink.wav"
-#define SOUND_DIT "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Dit.wav"
-#define SOUND_GLISS "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Gliss.wav"
-#define SOUND_LASER2 "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Laser.wav"
-#define SOUND_LOWWARP "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Lownote.wav"
-#define SOUND_WHIZ "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Whiz-1.wav"
-#define SOUND_WHIZ2 "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Whiz-2.wav"
-#define SOUND_PLUCK "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Pluck.wav"
-#define SOUND_LANG "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\1note7.wav"
-#define SOUND_BWAIOP "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\1note4.wav"
-#define SOUND_LOWBWAIOP "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\1note2.wav"
-#define SOUND_BEAT "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\1beat2.wav"
-#define SOUND_LOWBEAT "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\1beat1.wav"
-#define SOUND_BELL "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Ding2.wav"
-#define SOUND_BERP "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Hoot.wav"
-#define SOUND_LONGDING "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Longding.wav"
-#define SOUND_RINGNOTE "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Ringnote.wav"
-#define SOUND_QUICKDING "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Quikding.wav"
-#define SOUND_SYNTH "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Synth-3.wav"
-#define SOUND_WHISTLE "C:\\Documents and Settings\\nbkwdkv\\Desktop\\MyTool\\Sounds\\Whistle.wav"
+#define SOUND_TOAD "C:/TOAD/toadload.wav"
+#define SOUND_SWISH "C:/Documents and Settings/nbkwdkv/My Documents/Visual Studio 2008/Projects/ora/92010NT_CLT/autorun/click.wav"
+#define SOUND_DOINK "C:/blp/Wintrv/gecko/res/samples/test.wav"
+//#define SOUND_BWZZZ "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/fs_asterisk.wav"
+#define SOUND_TWDLZP "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/C801.wav"
+#define SOUND_WCHITA "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/C802.wav"
+#define SOUND_TWEEYAH "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/C809.wav"
+#define SOUND_BOODOOP "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/C810.wav"
+#define SOUND_DDDP "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/C813.wav"
+#define SOUND_LASER "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Laser SFX.wav"
+#define SOUND_CLINK "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Clink.wav"
+#define SOUND_DIT "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Dit.wav"
+#define SOUND_GLISS "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Gliss.wav"
+#define SOUND_LASER2 "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Laser.wav"
+#define SOUND_LOWWARP "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Lownote.wav"
+#define SOUND_WHIZ "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Whiz-1.wav"
+#define SOUND_WHIZ2 "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Whiz-2.wav"
+#define SOUND_PLUCK "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Pluck.wav"
+#define SOUND_LANG "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/1note7.wav"
+#define SOUND_BWAIOP "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/1note4.wav"
+#define SOUND_LOWBWAIOP "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/1note2.wav"
+#define SOUND_BEAT "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/1beat2.wav"
+#define SOUND_LOWBEAT "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/1beat1.wav"
+#define SOUND_BELL "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Ding2.wav"
+#define SOUND_BERP "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Hoot.wav"
+#define SOUND_LONGDING "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Longding.wav"
+#define SOUND_RINGNOTE "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Ringnote.wav"
+#define SOUND_QUICKDING "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Quikding.wav"
+#define SOUND_SYNTH "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Synth-3.wav"
+#define SOUND_WHISTLE "C:/Documents and Settings/nbkwdkv/Desktop/MyTool/Sounds/Whistle.wav"
+
+// Created by Windows XP Sound Recorder from 11Khz-16-stereo.  Same pitch, but pops at the beginning.  May be player.  More hiss added.
+#define SOUND_BWZZZ "C:/Tools/fs_asterisk.raw.44-16-stereo.wav" // Nasty pop
 
 enum EnumEventSound { EVS_CONNECT_SUCCEEDED, EVS_CONNECT_FAILED, EVS_EXECUTE_SUCCEEDED
-	, EVS_EXECUTE_FAILED };
+	, EVS_EXECUTE_FAILED, EVS_TEST_SUCCEEDED, EVS_TEST_FAILED };
 
 // WAVPCM
 // Wav to raw PCM converter
@@ -395,6 +392,12 @@ void Speak(EnumEventSound enumEventSound) {
 			PlaySound(SOUND_TWDLZP, NULL, SND_ASYNC|SND_NODEFAULT);
 			break;
 		case EVS_EXECUTE_FAILED:
+			PlaySound(SOUND_CLINK, NULL, SND_ASYNC|SND_NODEFAULT);
+			break;
+		case EVS_TEST_SUCCEEDED:
+			PlaySound(SOUND_TWDLZP, NULL, SND_ASYNC|SND_NODEFAULT);
+			break;
+		case EVS_TEST_FAILED:
 			PlaySound(SOUND_CLINK, NULL, SND_ASYNC|SND_NODEFAULT);
 			break;
 		default:
