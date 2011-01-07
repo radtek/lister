b1e3916c36c29393f43a94f9bcf78d6064fb3fbc
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-07 19:25:23 +0000

    - To be migrated to Urp lib soon - Detect control key and use to push straight from grid to script editor

--- src/lister/MyDropGrid.h
+++ src/lister/MyDropGrid.h
@@ -9,28 +9,53 @@
 struct MyDropGrid : public DropGrid {
 	typedef MyDropGrid CLASSNAME;
 	MyDropGrid() : DropGrid() {
-		list.WhenPopDown = THISBACK(SelectedItem);
+		list.WhenPopDown = THISBACK(SelectedScript);
 		list.WhenCursor = THISBACK(CursorChanged);
+		//list.WhenLeftClick = THISBACK(
 		list.DrawFocus().FocusLostAccepting();
+		ctrlKeyIsDown = false;
 	}
 
+	bool ctrlKeyIsDown;
 	//==========================================================================================	
 	virtual Image MouseEvent(int event, Point p, int zdelta, dword keyflags) {
+		if (event == Ctrl::MOUSELEAVE) {
+			ctrlKeyIsDown = false;
+		}
+		
+		if (keyflags & K_CTRL && event == Ctrl::LEFTDOWN) {
+			ctrlKeyIsDown = true;
+		} 
+		
+		if (event == Ctrl::LEFTUP) {
+			ctrlKeyIsDown = false;
+		}
+		
 		return DropGrid::MouseEvent(event, p, zdelta, keyflags);
 	}
 
 	//==========================================================================================	
 	void CursorChanged() {
-		int r = 0;
 	}
 
+	Callback WhenPushAddButton;
+		
 	//==========================================================================================	
-	void SelectedItem() {
-		Tip(Get(1).ToString()); 
+	MultiButton::SubButton& AddPlus(const Callback &cb) {
+		WhenPushAddButton = cb;
+		return DropGrid::AddPlus(cb);
+	}
+	
+	//==========================================================================================	
+	// User selected a script in the dropdown.
+	void SelectedScript() {
+		Tip(Get(1).ToString()); // Set the tip; may help to see full script with newlines.
+		if (GetCtrl()) { // !!(GetKeyState(VK_CONTROL) & 0x8000);
+			if (WhenPushAddButton) WhenPushAddButton(); // Simulate button push to avoid duping code.
+		}
 	}                 
 	
 	void Action() {
-		int h = 3;
 	}
 };
 
