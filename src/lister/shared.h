c6f244f0003d2e2798474b3b1adda8754ad4b796
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-07 19:35:46 +0000

    - Pushed some common constants here instead of the Constants.h file - Added reference to Urp lib which cleans out any references to GridCtrl or DropGrid - Put the CAT macro in Urp since I live by it - Added U++ database/sql helpers

--- src/lister/shared.h
+++ src/lister/shared.h
@@ -1,10 +1,7 @@
 #ifndef _shared_h_
 #define _shared_h_
 
-#include <CtrlLib/CtrlLib.h>
-#include <GridCtrl/GridCtrl.h>
-#include <DropGrid/DropGrid.h>
-
+#include <Urp/Urp.h>
 #include <PostgreSQL/PostgreSQL.h>
 
 using namespace Upp;
@@ -12,14 +9,32 @@ using namespace Upp;
 // This must be on this side of using namespace
 typedef PostgreSQLSession ControlSession;
 
-#define CAT String().Cat()
 #define UNKNOWN -1
+
+#define KEY_DECLAREDUNKNOWN  -1       // User must physically select this, attesting that we do not know
+#define KEY_MULTIMATCH       -2       // There are multiple values matching the input provided in our lookups, so we can't populate
+#define KEY_UNRESOLVED       -3       // The lookup string provided was not found in our reference data
+#define KEY_UNSET            -4       // Not set on an user entry screen, so we know they specifically did not set a value
+#define KEY_ATTRUNAVAIL      -5       // From a feed, the attribute was not presented
+#define KEY_SOURCEUNPOP      -6       // From a feed, the attribute was essentially empty, a blank, or a null
+#define KEY_SOURCEDEFLT      -7       // From a feed, the default value was provided, like "99999999" for a SSN, empirically determined
+#define KEY_MULTIVALUEDA     -8       // There are legitimately multiple values, stored in standard multivalue table type A
+
+#include "TestGrid.h"
 
 #define LAYOUTFILE <lister/lister.lay>
 #include <CtrlCore/lay.h>
 
 #include "image_shared.h"
 #include <Draw/iml_header.h>
+
+#define MODEL <lister/database.sch>
+
+#define SCHEMADIALECT <PostgreSQL/PostgreSQLSchema.h>
+#include "Sql/sch_header.h"
+
+#define SCHEMADIALECT <PostgreSQL/PostgreSQLSchema.h>
+#include "Sql/sch_source.h"
 
 #endif
 
