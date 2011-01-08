0337034a55a54d1568764cf21d507ab59653df88
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-08 01:31:07 +0000

    Split out code into cpp files to reduce U++ header scanning.

---
+++ trunk/src/Urp/UrpShared.cpp
@@ -0,0 +1,32 @@
+#include "UrpInput.h"
+
+bool In(int i, int in1, int in2) { if (i == in1 || i == in2) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3) {	if (i == in1 || i == in2 || i == in3) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4) { if (i == in1 || i == in2 || i == in3 || i == in4) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10, int in11) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10 || i == in11) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10, int in11, int in12) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10 || i == in11 || i == in12) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10, int in11, int in12, int in13) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10 || i == in11 || i == in12 || i == in13) return true; return false; }
+
+bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10, int in11, int in12, int in13, int in14) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10 || i == in11 || i == in12 || i == in13 || i == in14) return true; return false; }
+
+bool None(bool b1, bool b2) { return (!b1 && !b2); }
+bool None(bool b1, bool b2, bool b3) { return (!b1 && !b2 && !b3); }
+bool None(bool b1, bool b2, bool b3, bool b4) { return (!b1 && !b2 && !b3 && !b4); }
+bool None(bool b1, bool b2, bool b3, bool b4, bool b5) { return (!b1 && !b2 && !b3 && !b4 && !b5); }
