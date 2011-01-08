f2442c65623acb6413fbc5b623d58e48e0632ec3
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-08 22:07:36 +0000

    - Split layout out from shared.h since it causes problems occasionally.

--- src/lister/NewInstanceWin.h
+++ src/lister/NewInstanceWin.h
@@ -1,7 +1,8 @@
 #ifndef _NewInstanceWin_h_
 #define _NewInstanceWin_h_
 
-#include "shared.h"
+//#include "shared.h"
+#include "shared_layout.h"
 
 class NewInstanceWin : public WithNewInstanceLayout<TopWindow> {
 public:
