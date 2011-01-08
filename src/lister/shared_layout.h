b1d53b23c716c1018e7f6cf42b5768e6ecca0fe1
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-08 22:08:23 +0000

    - Split layout out from shared.h since it causes problems occasionally.

---
+++ src/lister/shared_layout.h
@@ -0,0 +1,11 @@
+#ifndef _lister_lister_shared_layout_h_
+#define _lister_lister_shared_layout_h_
+
+using namespace Upp;
+
+#include "TestGrid.h" // Referenced from layout
+
+#define LAYOUTFILE <lister/lister/lister.lay>
+#include <CtrlCore/lay.h>
+
+#endif
