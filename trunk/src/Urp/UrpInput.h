ef71235fe6ca23e30f60f01d5687c58896852a70
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-08 01:30:42 +0000

    Split out code into cpp files to reduce U++ header scanning.

--- trunk/src/Urp/UrpInput.h
+++ trunk/src/Urp/UrpInput.h
@@ -11,22 +11,6 @@ bool UrpInputBox(
 	,	String &splitOnThisToMacros // If inputed, each line is split by this string into [[MACRO1]], [[MACRO2]], etc.
 	,	bool &forceInputToUpper // Force input to upper, but not the wrapperText
 	,	bool &convertSpacesToUnderscores // Only on input
-	,	const char *title, const char *label) 
-{
-	WithMyEditStringLayout<TopWindow> dlg;
-	dlg.Sizeable().Zoomable();
-	CtrlLayoutOKCancel(dlg, title);
-	dlg.lbl = label;
-	dlg.text = wrapperText.ToWString();
-	if(dlg.Execute() == IDOK) {
-		wrapperText = dlg.text;
-		scrubAllOcc = dlg.fldScrubText;
-		splitOnThisToMacros = dlg.fldSplitOn;
-		forceInputToUpper = dlg.optForceInputToUpper;
-		convertSpacesToUnderscores = dlg.optConvertSpacesToUnderscores;
-		return true;
-	}
-	return false;
-}
+	,	const char *title, const char *label);
 
 #endif
