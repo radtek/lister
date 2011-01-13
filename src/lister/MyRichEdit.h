6589d20a7a53e8310a4414fbdb85f729f28bfe29
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:38:59 +0000

    - Added support for originalScriptId, so that if the user changes the script, a new script id is generated, but we need to support updating the old script record with the new text (shift key?) - Clean up confusion around script (plain text) and rich script, which mixing them causes corruption, loss of underscores and newlines, ect.

--- src/lister/MyRichEdit.h
+++ src/lister/MyRichEdit.h
@@ -50,8 +50,10 @@ struct MyRichEdit : public RichEdit {
 	Connection *connection;
 	int connId;  // connection id for this connection, so we can save it in configuration
 	int scriptId; // If brought from sqllist, then this is set.  Saved in configuration to make test creation easier
-
+	int originalScriptId; // If script content changes, this is set and scriptId is cleared
+	Callback WhenScriptContentChanged;  // Set from main to detect when to update the toolbar
 	double zoomlevel;
+	
 	enum PopupInfoRequestType {
 		IR_NOMATCH
 	,	IR_TABLESINSCHEMA
@@ -68,73 +70,44 @@ struct MyRichEdit : public RichEdit {
 	};
 	
 	MyRichEdit();
-	
 	//  Added a zoom text feature.
 	virtual void MouseWheel(Point p, int zdelta, dword keyflags);
-	
 	void Layout();
-	
 	void SetConn(Connection *mconnection);
-	
 	//  Turn a set of lines into a stream for IN clause use
 	void PasteWithApostrophe();
-
 	void PasteAsLineMacro();
-	
 	//  Turn a set of lines into series of text with the clipboard text pasted in
 	String PasteAsLineMacro(
 		Vector<String> lines, String macroMarker, String inBetweenNewLine, String title, 
 		String furtherInstructions = Null, String wrapperString = Null);
-	
 	//  Turn a set of lines into series of union alls (for ex:)
 	void PasteAsWrappedUnion();
-
 	// Came with the web search below.  Untested.	
 	static String DumpSpecial(String s);
-
 	// Doesn't work.
 	void SearchSharePoint();
-	
 	// Strip Qtf codes off of text.
 	void DeFormatSelection();
-
 	// Clean entire thing of Qtf.
 	void DeFormatScript();
-	
 	// Stolen from the RichEdit since I needed to manipulated non-exposed components.
 	virtual void RightDown(Point p, dword flags);
-
 	bool IsLegalWordChar(char cc);
-	
 	String GetPreviousWord(int &pc, WordTests &wordTests);
-	
 	void ProcessPaste();
-	
 	MyRichEdit::KeyProcessorResponse PopupRequested(bool ShowAllColumns);
-	
-	bool Key(dword key, int count);
-
-	//==========================================================================================	
+	virtual bool Key(dword key, int count);
 	// User selected an item from the popup grid, which closes it and sets the Cursor to the selected item.
 	// We now paste it into the script at the current script cursor.
 	void SelectedPopUpTable();
-
-	//==========================================================================================	
 	void SelectedPopUpColumn();
-
-	//==========================================================================================	
 	void AddColumns(String schema, String table);
-	
-	//==========================================================================================	
 	void AddTables(const String &schema);
-
-	//==========================================================================================	
-	void SetScript(String script);
-	//==========================================================================================	
-	// Script is always text, not Rich text.
-	String GetScript();
-
-	//==========================================================================================	
+	void ScriptContentChanged(dword key);
+	void SetScript(int scriptId, String script); // Script is always text, not Rich text.
+	void SetRichScript(int scriptId, String script); // Should create Rich (Qtf) string.
+	String GetScript(); // Returns plain text
 	// Save script id with script so "Add test for script" button works from toolbar.
 	virtual void Xmlize(XmlIO xml);
 	
