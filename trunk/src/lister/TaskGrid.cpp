de0437d91e3524c85ef0d563677c8f136d07eff2
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:43:07 +0000

    - Cosmetic - Wrapper for TASKID

--- trunk/src/lister/TaskGrid.cpp
+++ trunk/src/lister/TaskGrid.cpp
@@ -2,6 +2,7 @@
 #include "Connection.h"
 #include "shared_db.h"
 
+//==========================================================================================	
 TaskGrid::TaskGrid() {
 	SetTable(TASKS);
 	AddKey(TASKID);
@@ -11,13 +12,21 @@ TaskGrid::TaskGrid() {
 	AddColumn(NOTE, "tasknote", 100).Edit(taskNote);
 }
 
+//==========================================================================================	
 void TaskGrid::Build() {
 }
 
+//==========================================================================================	
 void TaskGrid::Load(Connection *pconnection) {
 	SetSession(*(pconnection->session));
 	SetSortColumn(TASKNAME);
 	Query();
-
 }
+
+//==========================================================================================	
+int TaskGrid::GetTaskId(int row) {
+	return Get(row, TASKID);
+}
+
+
 
