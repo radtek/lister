bbab061b7c906ab250d17a4bfabc0f62fe4f34fb
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:10:11 +0000

    - Captured U++ MSSQL and ODBC packages to override the Fetch and Execute to allow ExtendedFetch, a requirement for high-latency environments.

---
+++ trunk/src/ODBC/ODBC.cpp
@@ -0,0 +1,727 @@
+#include "ODBC.h"
+
+#include <CtrlLib/CtrlLib.h>
+#include <Sql/Sql.h>
+
+NAMESPACE_UPP
+
+#define LLOG(x) // LOG(x)
+
+// Set some tunable amount
+#define ROW_FETCH_COUNT 100
+#define MAX_COL_FETCH_COUNT 100
+#define MAX_DATA_WIDTH_FETCH 101
+
+struct MySqlColumnInfo : Moveable<MySqlColumnInfo> {
+	String      name;
+	int         type; // Value type in U++
+	SQLSMALLINT sqltype;
+	int         width;
+	int         precision; //number of total digits in numeric types
+	int         scale;     //number of digits after comma in numeric types
+	bool        nullable;  //true - column can hold null values
+	bool		isbinary;
+};
+
+class ODBCConnection : public SqlConnection
+{
+public:
+	ODBCConnection(ODBCSession *session);
+	virtual ~ODBCConnection();
+
+	void                    Clear();
+
+	virtual void            SetParam(int i, const Value& r);
+	virtual bool            Execute();
+	virtual int             GetRowsProcessed() const;
+	virtual bool            Fetch();
+	virtual void            GetColumn(int i, Ref r) const;
+	virtual void            Cancel();
+	virtual SqlSession&     GetSession() const { ASSERT(session); return *session; }
+	virtual String          GetUser() const    { ASSERT(session); return session->user; }
+	virtual String          ToString() const;
+	virtual Value           GetInsertedId() const;
+	Vector<MySqlColumnInfo>  myinfo;
+
+
+private:
+	friend class ODBCSession;
+
+	ODBCSession            *session;
+	struct Param {
+		int    ctype;
+		int    sqltype;
+		String data;
+		SQLLEN li;
+	};
+	Array<Param>             param, bparam;
+	String                   last_insert_table;
+
+	int                      rowsprocessed;
+	Vector< Vector<double> > number;
+	Vector< Vector<int64> >  num64;
+	Vector< Vector<String> > text;
+	Vector< Vector<Time> >   time;
+	int                      rowcount;
+	int                      rowi;
+	Vector<Value>            fetchrow;
+	Vector<bool>             binary;
+	
+	bool                     IsOk(SQLRETURN ret) const;
+	void                     Flush();
+	bool                     Fetch0();
+	bool                     IsCurrent() const   { return session->current == this; }
+	
+	// How many rows fetched in last ExtendedFetch call
+	SQLUINTEGER              rowsFetched;
+	// Irrelevant row status since we are not altering data
+	SQLUSMALLINT             rowStatus[ROW_FETCH_COUNT];
+	// Have we fetched a batch?  If not, fetch another for cycling through.
+	bool                     fetchedSet;
+	// Pointer within last fetched set for returning to caller
+	SQLSETPOSIROW            nextFetchSetRow;
+	// 100 cols x 100 rows x 100 characters.  TODO: Convert to dynamic allocation or cache
+	SQLCHAR                  fetchedRows[MAX_COL_FETCH_COUNT][ROW_FETCH_COUNT][MAX_DATA_WIDTH_FETCH];
+	// Tells us if the value was null
+	SQLLEN                   indicator[MAX_COL_FETCH_COUNT][ROW_FETCH_COUNT]; 
+};
+
+bool ODBCSession::Connect(const char *cs)
+{
+	if(henv && IsOk(SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc))) {
+		// Attempt to improve speed by making all connections read-only: TODO: Parameterize
+		SQLSetConnectAttr(hdbc, SQL_ATTR_ACCESS_MODE, (SQLPOINTER)SQL_MODE_READ_ONLY, SQL_NTS);
+		SQLSetConnectAttr(hdbc, SQL_ATTR_AUTOCOMMIT, (SQLPOINTER)SQL_AUTOCOMMIT_ON, SQL_NTS);
+		//SQLSetEnvAttr(env, SQL_ATTR_ODBC_VERSION, (void *) SQL_OV_ODBC3, 0);
+		// For BA work, serialized data is not required.  TODO: Parameterize
+		SQLSetConnectAttr(hdbc, SQL_ATTR_TXN_ISOLATION, (SQLPOINTER)SQL_TRANSACTION_READ_UNCOMMITTED, SQL_NTS);
+
+		if(IsOk(SQLDriverConnect(hdbc, NULL, (SQLCHAR *)cs, SQL_NTS, NULL, 0, NULL, SQL_DRIVER_NOPROMPT))) { // SQL_DRIVER_COMPLETE?
+			SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+			// Reference: http://msdn.microsoft.com/en-us/library/ms713605(VS.85).aspx
+//			//SQLSetConnectAttr(hdbc, SQL_ATTR_TXN_ISOLATION, (SQLPOINTER)SQL_TRANSACTION_SERIALIZABLE, SQL_NTS); // Took out, slowest connection
+			// See also: SQL_ATTR_CONNECTION_DEAD
+			return true;
+		}
+
+		// Connection failed if reached this point
+		SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+		hdbc = SQL_NULL_HANDLE;
+	}
+	return false;
+}
+
+bool ODBCSession::IsOpen() const
+{
+	return hdbc != SQL_NULL_HANDLE;
+}
+
+void ODBCSession::Close()
+{
+	if(hdbc != SQL_NULL_HANDLE) {
+		current = NULL;
+		FlushConnections();
+		SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+		SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+		hdbc = SQL_NULL_HANDLE;
+		hstmt = SQL_NULL_HANDLE;
+		current = NULL;
+	}
+}
+
+void ODBCSession::FlushConnections()
+{
+	LLOG("FlushConnections");
+	if(current) {
+		current->Flush();
+		current = NULL;
+	}
+	SQLFreeStmt(hstmt, SQL_CLOSE);
+}
+
+bool ODBCSession::IsOk(SQLRETURN ret)
+{
+	if(SQL_SUCCEEDED(ret))
+		return true;
+	SQLCHAR       SqlState[6], Msg[SQL_MAX_MESSAGE_LENGTH];
+	SQLINTEGER    NativeError;
+	SQLSMALLINT   MsgLen;
+	String        error;
+	int i = 1;
+	while(SQLGetDiagRec(SQL_HANDLE_DBC, hdbc, i++, SqlState, &NativeError,
+	                 Msg, sizeof(Msg), &MsgLen) != SQL_NO_DATA) {
+		if(error.GetCount())
+			error << "\r\n";
+		error << (char *)Msg;
+	}
+	SetError(error, statement);
+	return false;
+}
+
+ODBCSession::ODBCSession()
+{
+	hdbc = SQL_NULL_HANDLE;
+	hstmt = SQL_NULL_HANDLE;
+	SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+	if(henv)
+		SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (void *) SQL_OV_ODBC3, 0);
+	tlevel = 0;
+	Dialect(MSSQL);
+	current = NULL;
+}
+
+ODBCSession::~ODBCSession()
+{
+	if(henv)
+		SQLFreeHandle(SQL_HANDLE_ENV, henv);
+}
+
+void ODBCSession::Begin()
+{
+	if(tlevel == 0)
+		SQLSetConnectAttr(hdbc, SQL_ATTR_AUTOCOMMIT, (SQLPOINTER)SQL_AUTOCOMMIT_OFF, SQL_NTS);
+	tlevel++;
+}
+
+void ODBCSession::Commit()
+{
+	tlevel--;
+	ASSERT(tlevel >= 0);
+	if(tlevel == 0) {
+		SQLEndTran(SQL_HANDLE_DBC, hdbc, SQL_COMMIT);
+		SQLSetConnectAttr(hdbc, SQL_ATTR_AUTOCOMMIT, (SQLPOINTER)SQL_AUTOCOMMIT_ON, SQL_NTS);
+	}
+}
+
+void ODBCSession::Rollback()
+{
+	tlevel--;
+	ASSERT(tlevel >= 0);
+	if(tlevel == 0) {
+		SQLEndTran(SQL_HANDLE_DBC, hdbc, SQL_ROLLBACK);
+		SQLSetConnectAttr(hdbc, SQL_ATTR_AUTOCOMMIT, (SQLPOINTER)SQL_AUTOCOMMIT_ON, SQL_NTS);
+	}
+}
+
+String ODBCSession::Savepoint()
+{
+	NEVER();
+	return "";
+}
+
+void ODBCSession::RollbackTo(const String& savepoint)
+{
+	NEVER();
+}
+
+Vector<String> ODBCSession::EnumUsers()
+{
+	return Vector<String>();
+}
+
+Vector<String> ODBCSession::EnumDatabases()
+{
+	return Vector<String>();
+}
+
+Vector<String> ODBCSession::EnumTables(String database)
+{
+	return Vector<String>();
+}
+
+Vector<String> ODBCSession::EnumViews(String database)
+{
+	return Vector<String>();
+}
+
+Vector<String> ODBCSession::EnumSequences(String database)
+{
+	return Vector<String>();
+}
+
+Vector<String> ODBCSession::EnumPrimaryKeys(String database, String table)
+{
+	return Vector<String>();
+}
+
+String ODBCSession::EnumRowID(String database, String table)
+{
+	return "";
+}
+
+bool   ODBCPerformScript(const String& text, StatementExecutor& executor, Gate2<int, int> progress_canceled)
+{
+	const char *p = text;
+	while(*p) {
+		String cmd;
+		while(*p && *p != ';')
+			if(*p == '\'') {
+				const char *s = p;
+				while(*++p && (*p != '\'' || *++p == '\''))
+					;
+				cmd.Cat(s, int(p - s));
+			}
+			else {
+				if(*p > ' ')
+					cmd.Cat(*p);
+				else if(!cmd.IsEmpty() && *cmd.Last() != ' ')
+					cmd.Cat(' ');
+				p++;
+			}
+		if(progress_canceled(int(p - text.Begin()), text.GetLength()))
+			return false;
+		if(!IsNull(cmd) && !executor.Execute(cmd))
+			return false;
+		if(*p == ';')
+			p++;
+	}
+	return true;
+}
+
+SqlConnection *ODBCSession::CreateConnection()
+{
+	return new ODBCConnection(this);
+}
+
+ODBCConnection::ODBCConnection(ODBCSession *session_)
+:	session(session_)
+{
+	LLOG("ODBCConnection " << (void *)this << " " << (void *)session);
+	rowcount = rowi = 0;
+}
+
+ODBCConnection::~ODBCConnection()
+{
+	if(IsCurrent())
+		session->current = NULL;
+	LLOG("~ODBCConnection " << (void *)this << " " << (void *)session);
+}
+
+bool ODBCConnection::IsOk(SQLRETURN ret) const
+{
+	if(SQL_SUCCEEDED(ret) || ret == SQL_NO_DATA)
+		return true;
+	SQLCHAR       SqlState[6], Msg[SQL_MAX_MESSAGE_LENGTH];
+	SQLINTEGER    NativeError;
+	SQLSMALLINT   MsgLen;
+	String        error;
+	int i = 1;
+	while(SQLGetDiagRec(SQL_HANDLE_STMT, session->hstmt, i++, SqlState, &NativeError,
+	                    Msg, sizeof(Msg), &MsgLen) != SQL_NO_DATA) {
+		if(error.GetCount())
+			error << "\r\n";
+		error << (char *)Msg;
+	}
+	session->SetError(error, statement);
+	return false;
+}
+
+void ODBCConnection::SetParam(int i, const Value& r)
+{
+	Param& p = param.At(i);
+	if(IsNumber(r)) {
+		if(r.Is<int64>()) {
+			int64 x = r;
+			p.ctype = SQL_C_SBIGINT;
+			p.sqltype = SQL_BIGINT;
+			p.data = String((char *)&x, sizeof(x));
+			p.li = sizeof(x);
+		}
+		else {
+			double x = r;
+			p.ctype = SQL_C_DOUBLE;
+			p.sqltype = SQL_DOUBLE;
+			p.data = String((char *)&x, sizeof(x));
+			p.li = sizeof(x);
+		}
+	}
+	if(IsString(r)) {
+		p.ctype = SQL_C_CHAR;
+		p.sqltype = SQL_LONGVARCHAR;
+		p.data = r;
+		p.li = p.data.GetLength();
+	}
+	if(IsDateTime(r)) {
+		p.ctype = SQL_C_TYPE_TIMESTAMP;
+		p.sqltype = SQL_TYPE_TIMESTAMP;
+		Time t = r;
+		SQL_TIMESTAMP_STRUCT tm;
+		tm.year = t.year;
+		tm.month = t.month;
+		tm.day = t.day;
+		tm.hour = t.hour;
+		tm.minute = t.minute;
+		tm.second = t.second;
+		tm.fraction = 0;
+		p.data = String((char *)&tm, sizeof(tm));
+		p.li = sizeof(tm);
+	}
+	if(IsNull(r))
+		p.li = SQL_NULL_DATA;
+}
+
+//==============================================================================================
+// New version: Supports extendedfetch: results in 1000% increase in performance over latent connections.
+bool ODBCConnection::Execute()
+{
+	LLOG("Execute " << (void *)this << " " << (void *)session);
+	if(session->hstmt == SQL_NULL_HANDLE)
+		return false;
+	if(IsCurrent())
+		session->current = NULL;
+	session->FlushConnections();
+	last_insert_table.Clear();
+	number.Clear();
+	text.Clear();
+	time.Clear();
+	CParser p(statement);
+	if((p.Id("insert") || p.Id("INSERT")) && (p.Id("into") || p.Id("INTO")) && p.IsId())
+		last_insert_table = p.ReadId();
+	
+	// Set up the call to ExtendedFetch.  Required SQLBindCol, cannot use SQLGetData.
+	int rtnc = SQLSetStmtOption (session->hstmt, SQL_ROWSET_SIZE, ROW_FETCH_COUNT);
+	fetchedSet = false;
+	if(!IsOk(SQLPrepare(session->hstmt, (SQLCHAR *)~statement, statement.GetCount())))
+		return false;
+	parse = false;
+	bparam = param;
+	param.Clear();
+	for(int i = 0; i < bparam.GetCount(); i++) {
+		Param& p = bparam[i];
+		SQLSMALLINT     DataType;
+		SQLULEN         ParameterSize;
+		SQLSMALLINT     DecimalDigits;
+		SQLSMALLINT     Nullable;
+		if(!IsOk(SQLDescribeParam(session->hstmt, i + 1, &DataType, &ParameterSize, &DecimalDigits, &Nullable)))
+			return false;
+		if(!IsOk(SQLBindParameter(session->hstmt, i + 1, SQL_PARAM_INPUT, p.ctype, DataType,
+		                          ParameterSize, DecimalDigits, (SQLPOINTER)~p.data, p.data.GetLength(),
+		                          &p.li)))
+			return false;
+	}
+
+	SQLSMALLINT ncol;
+	
+	if(!IsOk(SQLExecute(session->hstmt)) || !IsOk(SQLNumResultCols(session->hstmt, &ncol))) {
+		SQLFreeStmt(session->hstmt, SQL_CLOSE);
+		return false;
+	}
+	session->current = this;
+	myinfo.Clear();
+	binary.Clear();
+	
+	// Identify all properties of returned data and bind space for fetching
+	
+	for(int i = 1; i <= ncol; i++) {
+		SQLCHAR      ColumnName[256];
+		SQLSMALLINT  NameLength;
+		SQLSMALLINT  DataType;
+		SQLULEN      ColumnSize;
+		SQLSMALLINT  DecimalDigits;
+		SQLSMALLINT  Nullable;
+		
+		if(!IsOk(SQLDescribeCol(session->hstmt, i, ColumnName, 255, &NameLength, &DataType,
+		                        &ColumnSize, &DecimalDigits, &Nullable)))
+			return false;
+		binary.Add(false);
+		MySqlColumnInfo& f = myinfo.Add();
+		SqlColumnInfo flegacy = info.Add(); // Maintain old version ;)
+		
+		flegacy.nullable = f.nullable = Nullable != SQL_NO_NULLS;
+		flegacy.precision = f.precision = DecimalDigits;
+		flegacy.scale = f.scale = 0;
+		flegacy.width = f.width = ColumnSize;
+		flegacy.name = f.name = (char *)ColumnName;
+		
+		// My new property
+		f.sqltype = DataType;
+		f.isbinary = false;
+		
+		switch(DataType) {
+		case SQL_DECIMAL:
+		case SQL_NUMERIC:
+		case SQL_SMALLINT:
+		case SQL_INTEGER:
+		case SQL_REAL:
+		case SQL_FLOAT:
+		case SQL_DOUBLE:
+		case SQL_BIT:
+		case SQL_TINYINT:
+			f.type = DOUBLE_V;
+			break;
+		case SQL_BIGINT:
+			f.type = INT64_V;
+			break;
+		case SQL_TYPE_DATE:
+			f.type = DATE_V;
+			if (!IsOk(SQLBindCol(session->hstmt, i, SQL_C_TYPE_DATE, fetchedRows[i-1], sizeof(SQL_DATE_STRUCT), indicator[i-1]))) return false;
+			break;
+		case SQL_TYPE_TIMESTAMP:
+			f.type = TIME_V;
+			// BUG:
+			if (!IsOk(SQLBindCol(session->hstmt, i, SQL_C_TYPE_TIMESTAMP, fetchedRows[i-1], 101, indicator[i-1]))) return false;
+			break;
+		case SQL_BINARY:
+		case SQL_VARBINARY:
+		case SQL_LONGVARBINARY:
+			f.type = STRING_V;
+			f.isbinary = true;
+			binary.Top() = true;
+			break;
+		default:
+			f.type = STRING_V;
+			if (!IsOk(SQLBindCol(session->hstmt, i, SQL_C_CHAR, fetchedRows[i-1], 101, indicator[i-1]))) return false;
+			break;
+		}
+		
+		flegacy.type = f.type; // Copy to legacy
+		
+	}
+	
+	SQLLEN rc;
+	SQLRowCount(session->hstmt, &rc);
+	rowsprocessed = rc;
+	return true;
+}
+
+//==============================================================================================
+int ODBCConnection::GetRowsProcessed() const
+{
+	return rowsprocessed;
+}
+
+//==============================================================================================
+// Extended support from U++ version: High-performance fetch
+bool ODBCConnection::Fetch0()
+{
+	LLOG("Fetch0 " << (void *)this << " " << (void *)session);
+	int ret;
+	
+	// Fetch a batch set for reduced latency (huge difference)
+	if (!fetchedSet) {
+		ret = SQLExtendedFetch(session->hstmt, SQL_FETCH_NEXT, 0, &rowsFetched, rowStatus);
+		if(ret == SQL_NO_DATA || !IsOk(ret))
+			return false;
+		fetchedSet = true; // So we don't run until we've returned all these
+		nextFetchSetRow = 0; // SQLSetPos is 1-based, but return data is 0-based!
+	}
+
+	fetchrow.Clear();
+	double dbl;
+	int64 n64;
+	// Data Types Reference: http://msdn.microsoft.com/en-us/library/ms714556(v=vs.85).aspx
+	SQL_TIMESTAMP_STRUCT tmstmp;
+	SQL_DATE_STRUCT dt;
+	SQL_TIME_STRUCT tm;
+	SQLLEN li;
+	SQLGUID guid;
+	SQL_NUMERIC_STRUCT n;
+	SQL_INTERVAL_STRUCT iv;
+	SQLREAL rl; // Same as float
+	SQLFLOAT flt; // Same as double
+	SQLINTEGER lint; // long int
+	
+	// Walk through each column
+	
+	for(int i = 0; i < myinfo.GetCount(); i++) {
+		Value v = Null;
+
+		if (indicator[i][nextFetchSetRow] != SQL_NULL_DATA) {
+			switch(myinfo[i].type) {
+
+			case DOUBLE_V:
+				memcpy((void *)&dbl, (void *)fetchedRows[i][nextFetchSetRow], sizeof(dbl));
+				v = dbl;
+				break;
+
+			case INT64_V:
+				memcpy((void *)&n64, (void *)fetchedRows[i][nextFetchSetRow], sizeof(n64));
+				v = n64;
+				break;
+	
+			case DATE_V:
+				memcpy((void *)&dt, (void *)fetchedRows[i][nextFetchSetRow], sizeof(dt));
+				{
+					Date d;
+					d.day = (byte)dt.day;
+					d.month = (byte)dt.month;
+					d.year = dt.year;
+					v = d;
+				}
+				break;
+				
+			case TIME_V: // Same for timestamps and date SQL Server types
+				memcpy((void *)&tmstmp, (void *)fetchedRows[i][nextFetchSetRow], sizeof(tmstmp));
+				{	
+					Time m;
+					m.year = tmstmp.year;
+					m.month = (byte)tmstmp.month;
+					m.day = (byte)tmstmp.day;
+					m.hour = (byte)tmstmp.hour;
+					m.minute = (byte)tmstmp.minute;
+					m.second = (byte)tmstmp.second;
+					v = m;
+				}
+				break;
+
+			default:
+				if (indicator[i][nextFetchSetRow] == SQL_NULL_DATA) {
+					v = Null;
+				} else {
+					v = String((char *)fetchedRows[i][nextFetchSetRow]);
+				}
+				break;
+			}
+		}
+		
+		fetchrow.Add(v);
+	}
+	
+	nextFetchSetRow++;
+	
+	if (nextFetchSetRow >= rowsFetched) {
+		fetchedSet = false;
+	}
+	
+	// When SQLFetch signals the end of the result set by returning SQL_NO_DATA, call SQLMoreResults to determine if another result set is available. 
+	// To cancel processing a result set before SQLFetch returns SQL_NO_DATA, call SQLCloseCursor
+	return true;
+}
+
+bool ODBCConnection::Fetch()
+{
+	if(IsCurrent())
+		return Fetch0();
+	if(rowi >= rowcount)
+		return false;
+	fetchrow.Clear();
+	for(int i = 0; i < myinfo.GetCount(); i++) {
+		Value v;
+		switch(myinfo[i].type) {
+		case DOUBLE_V:
+			v = number[i][rowi];
+			break;
+		case INT64_V:
+			v = num64[i][rowi];
+			break;
+		case TIME_V:
+			v = time[i][rowi];
+			break;
+		default:
+			v = text[i][rowi];
+			break;
+		}
+		fetchrow.Add(v);
+	}
+	++rowi;
+	return true;
+}
+
+void ODBCConnection::GetColumn(int i, Ref r) const
+{
+	LLOG("GetColumn " << (void *)this << " " << (void *)session);
+	r.SetValue(fetchrow[i]);
+}
+
+void ODBCConnection::Flush()
+{
+	LLOG("Flush " << (void *)this);
+	rowcount = 0;
+	rowi = 0;
+	number.Clear();
+	number.SetCount(myinfo.GetCount());
+	text.Clear();
+	text.SetCount(myinfo.GetCount());
+	time.Clear();
+	time.SetCount(myinfo.GetCount());
+	while(myinfo.GetCount() && Fetch0()) {
+		rowcount++;
+		for(int i = 0; i < myinfo.GetCount(); i++)
+			switch(myinfo[i].type) {
+			case DOUBLE_V:
+				number[i].Add(fetchrow[i]);
+				break;
+			case INT64_V:
+				num64[i].Add(fetchrow[i]);
+				break;
+			case STRING_V:
+				text[i].Add(fetchrow[i]);
+				break;
+			case TIME_V:
+				time[i].Add(fetchrow[i]);
+				break;
+			}
+	}
+	LLOG("Flush fetched " << rowcount);
+}
+
+void ODBCConnection::Cancel()
+{
+	param.Clear();
+	bparam.Clear();
+	number.Clear();
+	text.Clear();
+	time.Clear();
+}
+
+String ODBCConnection::ToString() const
+{
+	return statement;
+}
+
+Value ODBCConnection::GetInsertedId() const
+{
+	Sql sql(GetSession());
+	return last_insert_table.GetCount() ? sql.Select("IDENT_CURRENT('" + last_insert_table + "')")
+	                                    : sql.Select("@@IDENTITY");
+}
+
+END_UPP_NAMESPACE
+
+/*
+Copy #define ROWS 20
+#define STATUS_LEN 6
+
+SQLCHAR        szStatus[ROWS][STATUS_LEN], szReply[3];
+SQLINTEGER     cbStatus[ROWS], cbOrderID;
+SQLUSMALLINT   rgfRowStatus[ROWS];
+SQLUINTEGER    sOrderID, crow = ROWS, irow;
+SQLHSTMT       hstmtS, hstmtU;
+
+SQLSetStmtAttr(hstmtS, SQL_ATTR_CONCURRENCY, (SQLPOINTER) SQL_CONCUR_ROWVER, 0);
+SQLSetStmtAttr(hstmtS, SQL_ATTR_CURSOR_TYPE, (SQLPOINTER) SQL_CURSOR_KEYSET_DRIVEN, 0);
+SQLSetStmtAttr(hstmtS, SQL_ATTR_ROW_ARRAY_SIZE, (SQLPOINTER) ROWS, 0);
+SQLSetStmtAttr(hstmtS, SQL_ATTR_ROW_STATUS_PTR, (SQLPOINTER) rgfRowStatus, 0);
+SQLSetCursorName(hstmtS, "C1", SQL_NTS);
+SQLExecDirect(hstmtS, "SELECT ORDERID, STATUS FROM ORDERS ", SQL_NTS);
+
+SQLBindCol(hstmtS, 1, SQL_C_ULONG, &sOrderID, 0, &cbOrderID);
+SQLBindCol(hstmtS, 2, SQL_C_CHAR, szStatus, STATUS_LEN, &cbStatus);
+
+while ((retcode == SQLFetchScroll(hstmtS, SQL_FETCH_NEXT, 0)) != SQL_ERROR) {
+   if (retcode == SQL_NO_DATA_FOUND)
+      break;
+   for (irow = 0; irow < crow; irow++) {
+      if (rgfRowStatus[irow] != SQL_ROW_DELETED)
+         printf("%2d %5d %*s\n", irow+1, sOrderID, NAME_LEN-1, szStatus[irow]);
+   }
+   while (TRUE) {
+      printf("\nRow number to update?");
+      gets_s(szReply, 3);
+      irow = atoi(szReply);
+      if (irow > 0 && irow <= crow) {
+         printf("\nNew status?");
+         gets_s(szStatus[irow-1], (ROWS * STATUS_LEN));
+         SQLSetPos(hstmtS, irow, SQL_POSITION, SQL_LOCK_NO_CHANGE);
+         SQLPrepare(hstmtU,
+          "UPDATE ORDERS SET STATUS=? WHERE CURRENT OF C1", SQL_NTS);
+         SQLBindParameter(hstmtU, 1, SQL_PARAM_INPUT,
+            SQL_C_CHAR, SQL_CHAR,
+            STATUS_LEN, 0, szStatus[irow], 0, NULL);
+         SQLExecute(hstmtU);
+      } else if (irow == 0) {
+         break;
+      }
+   }
+}
+*/
