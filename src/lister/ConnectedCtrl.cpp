a7cf6b0529cfe2fc2576336f10e10ab12f3964f1
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:46:41 +0000

    - Cosmetic

--- src/lister/ConnectedCtrl.cpp
+++ src/lister/ConnectedCtrl.cpp
@@ -1,12 +1,13 @@
 #include "ConnectedCtrl.h"
 #include "Connection.h"
+#include "ConnState.h" // CON_SUCCEED definition
 
-#include "ConnState.h"
-
+//==========================================================================================	
 ConnectedCtrl::ConnectedCtrl() {
 	connection = NULL;
 }
 
+//==========================================================================================	
 /*virtual*/ void ConnectedCtrl::Load(Connection *pconnection) {
 	ASSERT(pconnection);
 	ASSERT(pconnection->enumConnState == CON_SUCCEED);
