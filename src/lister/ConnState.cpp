bae96be03d0f78372612a227ed2c4912569c06e3
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:54:37 +0000

    - Stripped indent

--- src/lister/ConnState.cpp
+++ src/lister/ConnState.cpp
@@ -1,26 +1,28 @@
 #include "ConnState.h"
 
-	ConnState::ConnState() : ColorPusher() {
-		//	rgbactrl <<= THISBACK(ColorChanged);
+//==========================================================================================	
+ConnState::ConnState() : ColorPusher() {
+	//	rgbactrl <<= THISBACK(ColorChanged);
+}
+
+//==========================================================================================	
+/*static*/ Color ConnState::ConvertStateToColor(EnumConnState enumConnState) {
+	
+	switch (enumConnState) {
+		case NOCON_NEVER: return Gray();
+		case NOCON_WASSUCC: return Green();
+		case NOCON_WASFAIL: return Red();
+		case NOCON_UNDEF: return LtGray();
+		case NOCON_MISCONFIG: return Magenta();
+		case CONNECTING_START: return LtYellow();
+		case CONNECTING_YAWN: return Yellow();
+		case CONNECTING_2NDTRY: return LtMagenta();
+		case CONNECTING_3RDTRY: return White();
+		case CONNECTING_TIMEOUT: return Cyan();
+		case CON_SUCCEED: return LtGreen();
+		case CON_FAIL: return LtRed();
+		case CON_STALE: return LtBlue();
 	}
 	
-	/*static*/ Color ConnState::ConvertStateToColor(EnumConnState enumConnState) {
-		
-		switch (enumConnState) {
-			case NOCON_NEVER: return Gray();
-			case NOCON_WASSUCC: return Green();
-			case NOCON_WASFAIL: return Red();
-			case NOCON_UNDEF: return LtGray();
-			case NOCON_MISCONFIG: return Magenta();
-			case CONNECTING_START: return LtYellow();
-			case CONNECTING_YAWN: return Yellow();
-			case CONNECTING_2NDTRY: return LtMagenta();
-			case CONNECTING_3RDTRY: return White();
-			case CONNECTING_TIMEOUT: return Cyan();
-			case CON_SUCCEED: return LtGreen();
-			case CON_FAIL: return LtRed();
-			case CON_STALE: return LtBlue();
-		}
-		
-		return Black();
-	}
+	return Black();
+}
