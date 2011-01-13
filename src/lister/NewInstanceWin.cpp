d1ef3db8f5e545e8df7b69ecbe4e64ca67acf40b
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:39:39 +0000

    - Cosmetic

--- src/lister/NewInstanceWin.cpp
+++ src/lister/NewInstanceWin.cpp
@@ -1,5 +1,6 @@
 #include "NewInstanceWin.h"
 
+//==========================================================================================	
 NewInstanceWin::NewInstanceWin() {
 	CtrlLayout(*this, "Create new instance (database)");
 	CtrlLayoutOKCancel(*this, "Enter");
