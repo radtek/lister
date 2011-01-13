c0ab6562c1f7038df3bd05486b8b5bb97b5b06f0
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:19:58 +0000

    - Added functionality to store and present the last inserted sequence # for the primary key column via a call back.  clumsy.

--- src/Urp/UrpSqlGrid.h
+++ src/Urp/UrpSqlGrid.h
@@ -3,54 +3,36 @@
 
 #include "UrpShared.h"
 
-#define HIDDEN_COLUMN -2
+#define HIDDEN_COLUMN -2 // TODO: Being used?
 
-//==========================================================================================	
 class UrpSqlGrid: public SqlArray {
 public:
-	//==========================================================================================	
+	int lastinsertedpkid;
+	Callback WhenNeedLastInsertedPkId;
 	UrpSqlGrid();
-
-	//==========================================================================================	
+	// Override since the standard function expects the pk to be named "id" in order to fetch
+	// from Postgre sequence.
+	virtual bool  UpdateRow();
 	// Obviously, U++ developer Uno was being a total dickwad when he refused to give a function
 	// to extract column widths, so I've created it.
 	int GetFloatingColumnWidth(int colno);
-	
-	//==========================================================================================	
 	void SetSortColumn(Id id, bool desc = false);
-	
-	//==========================================================================================	
 	int GetColumnNo(Id id);
-	
-	//==========================================================================================	
 	// Series of corrective functions to deal with Lamo Uno's confusion about when and when not to adjust references for fixed columns
 	int GetFloatingColumnCount();
-	
-	//==========================================================================================	
 	void UnhideFloatingColumnSilently(int i);
-	
-	//==========================================================================================	
 	void HideFloatingColumn(int i);
-	
-	//==========================================================================================	
+	void HideColumn(Id id); // TODO: Clean up names
 	void SetFloatingColumnWidth(int i, int w);
-	
-	//==========================================================================================	
 	Id GetFloatingColumnId(int n) const;
-	
-	//==========================================================================================	
 	bool IsColumnHidden(int i);
-	
-	//==========================================================================================	
 	void SetColumnMinWidth(int i, int w);
-	
-//	GridCtrl::ItemRect& GetFloatingColumn(int n);
-	
-	//==========================================================================================	
+	//	GridCtrl::ItemRect& GetFloatingColumn(int n);
 	//  GridCtrl is supposed to Xmlize, but I don't see it doing anything, so I've written my own.
 	//  Have to save by name so that code changes that add/subtract columns will not cause confusion.
 	//  Also, we save hidden state by name instead of position, which can really mess with a grid.
 	void Xmlize(XmlIO xml);
+	
 };
 
 #endif
