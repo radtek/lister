112ce496a95823d546a0126889ada08dfe6240fa
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:46:18 +0000

    - Cosmetic

--- trunk/src/lister/ConnButton.cpp
+++ trunk/src/lister/ConnButton.cpp
@@ -1,10 +1,12 @@
 #include "ConnButton.h"
 
+//==========================================================================================	
 ConnButton::ConnButton() : Button() {
 	WhenPush = THISBACK(Connect); // Forces this to be a nested class
 	wasClicked = false;
 }
 
+//==========================================================================================	
 // Only called if we use WhenCtrlsAction, NOT WhenCtrlAction.	
 void ConnButton::Connect() {
 	wasClicked = true;
