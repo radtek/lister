923a96bb3367c892a74cd1d4d80c7b83d06b42b7
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:41:41 +0000

    - ScriptGrid has really become "Scripts attached to the tasks listed to the left".  Should change name but oh heck. - Added TASKID linkage using U++ SqlArray support for joining. - Added method LinkScriptToTaskGrid to link the two grids together in master-detail view - Wrappers for scriptId

--- trunk/src/lister/ScriptGrid.cpp
+++ trunk/src/lister/ScriptGrid.cpp
@@ -2,21 +2,43 @@
 #include "Connection.h"
 #include "shared_db.h"
 
+//==========================================================================================	
 ScriptGrid::ScriptGrid() {
-	SetTable(SCRIPTS);
-	AddKey(SCRIPTID);
+	SetTable(TASKS_R); // All relations to a task, regardless of type
+	AddKey(RELID);
+	AddColumn(TASKID, "taskid", 50);
+	HideColumn(TASKID); // For joining to task grid only.
+	AddColumn(WHY, "why", 160).Edit(why);
+	AddColumn(SCRIPT, "script", 200);
 	AddColumn(SCRIPTNAME, "script", 140).Edit(scriptName);
-	AddColumn(NOTE, "note", 200).Edit(note);
-	AddColumn(SCRIPT, "body", 200).Edit(scriptBody);
+	AddColumn(SCRIPTNOTE, "script note", 200).Edit(scriptNote);
+	AddColumn(SCRIPTID, "scriptid", 50);
 }
 
+//==========================================================================================	
 void ScriptGrid::Build() {
 }
 
+//==========================================================================================	
 void ScriptGrid::Load(Connection *pconnection) {
 	SetSession(*(pconnection->session));
 	SetSortColumn(SCRIPTNAME);
 	Query();
+}
 
+//==========================================================================================	
+// Link Many table (this) to one table
+void ScriptGrid::LinkScriptToTaskGrid(ArrayCtrl &taskGrid) {
+	Join(TASKID, taskGrid);
+}
+
+//==========================================================================================	
+int ScriptGrid::GetScriptId(int row) {
+	return Get(row, SCRIPTID);
+}
+
+//==========================================================================================	
+String ScriptGrid::GetScript(int row) {
+	return Get(row, SCRIPT);
 }
 
