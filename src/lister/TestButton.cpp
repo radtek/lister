ef0aadd53d6e1986cd61ff9f730bfc5758447a18
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-13 20:43:46 +0000

    - Cosmetic

--- src/lister/TestButton.cpp
+++ src/lister/TestButton.cpp
@@ -1,10 +1,12 @@
 #include "TestButton.h"
 
+//==========================================================================================	
 TestButton::TestButton() : Button() {
 	WhenPush = THISBACK(BeginTest); // Forces this to be a nested class
 	wasClicked = false;
 }
 
+//==========================================================================================	
 // Only called if we use WhenCtrlsAction, NOT WhenCtrlAction.	
 void TestButton::BeginTest() {
 	wasClicked = true; // TestGrid will pick up
