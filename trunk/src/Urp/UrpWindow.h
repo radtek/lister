525ae1ab6164681b257798895072f3c2055d893c
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 17:39:36 +0000

    - Added some thoughtful helpers.  For windows created dynamically, we support a map to controls attached to this window, so they don't get lost in space. - Need to zap out the SerializePlacement since not used anymore. - Woohoo! Xmlize now positions windows properly - Created vector for dynamic window creation.  Works, but positioning is way off for UrpChild - CloseIfNotClosed, OpenIfNotOpen are cool functions I've always wanted.  Why does Open crash if its open?? - Moved binConfigFile and xml... out of TopWindow so they can be per Window.

--- trunk/src/Urp/UrpWindow.h
+++ trunk/src/Urp/UrpWindow.h
@@ -3,28 +3,65 @@
 
 #include "UrpShared.h"
 
+//==============================================================================================
 class UrpWindow : public TopWindow {
 public:
+	Vector<Ctrl *> ctrls;
 	enum { MINIMIZED, MAXIMIZED, OVERLAPPED };
 	Rect        overlapped;
 	byte        state;
+	String      configFileNameBase;
+	String      binConfigFile;
+	String      xmlConfigFile;
+	Rect realPosition;
 
 	UrpWindow();
 	virtual void Run(bool appmodal = false);
+	void AddCtrl(Ctrl *ctrl);
+	void Serialize(Stream& s);
 	void SerializePlacement(Stream& s, bool reminimize = false);
+	virtual void Xmlize(XmlIO xml);
+	virtual void Close();
+	virtual void Open();
+	virtual bool OpenIfNotOpen();
+	virtual bool CloseIfNotClosed();
 };
 
+//==============================================================================================
 class UrpTopWindow : public UrpWindow {
 public:
 	String		exeFilePath;
 	String		connectAsUser;
 	String		configFileFolder;
-	String binConfigFile;
-	String xmlConfigFile;
 	
 	UrpTopWindow();
 	
 	virtual void Run(bool appmodal = false);
 };
 
+//==============================================================================================
+class UrpConfigWindow : public UrpWindow {
+public:
+	String                 configName;
+	UrpTopWindow          *topWindow;
+
+	// Construct a configurable window; take config path from topWindow
+	UrpConfigWindow(UrpTopWindow *ptopWindow, String pconfigName);
+	// Load configuration file based on 
+	virtual void Open();
+	virtual void Close();
+};
+
+
+//==============================================================================================
+// Generator for dynamic creation of configurated windows
+class UrpWindowFactory {
+public:
+	static VectorMap<String, UrpConfigWindow *>& Windows();
+	UrpWindowFactory();
+	~UrpWindowFactory();
+	UrpConfigWindow *Open(UrpTopWindow *ptopWindow, String pwindowName); // Create and open window if not open, then show it
+};
+
 #endif
+
