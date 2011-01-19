2fb9e1e4e31d139b0ca847feee279cfaf84387a3
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 17:34:04 +0000

    - Attempting to enhance splitter to remember its state for when I support double-click zoom.  Not done.

--- src/Urp/UrpSplitter.cpp
+++ src/Urp/UrpSplitter.cpp
@@ -1,17 +1,34 @@
 #include "UrpSplitter.h"
 
 //==========================================================================================	
+UrpSplitter::UrpSplitter() : Splitter() {
+	savedSplitterPosition = -1;
+	isExpanded = isShrunk = false;
+}
+
+//==========================================================================================	
 void UrpSplitter::Xmlize(XmlIO xml) {
 	int a;
+
+	xml
+		("isexpanded", isExpanded)
+		("isshrunk", isShrunk)
+		("savedpos", savedSplitterPosition)
+	;
 	
-	if(xml.IsLoading()) {
-		xml("pos", a);
+	if (xml.IsLoading()) {
+		xml
+		    ("pos", a)
+		;
+		
 		if (a <= 20) a = 200;
 		SetPos(a);
 	} else {
 		// Storing
 		a = GetPos();
-		xml("pos", a);
+		xml
+			("pos", a)
+		;
 	}
 }
 
