cb7926853f5d5c08e7f429566fa3a5e463b0e2bd
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 17:51:40 +0000

    - Functionality replaced by UrpWindow

--- src/lister/TestWin.cpp
+++ src/lister/TestWin.cpp
@@ -1,44 +1,7 @@
 #include "TestWin.h"
 
-//==========================================================================================	
+//==============================================================================================
 TestWin::TestWin() {
 	CtrlLayout(*this, "Create new Test");
 	Sizeable().Zoomable();
-}
-
-//==========================================================================================	
-void TestWin::Open() {
-	TopWindow::Open();
-	SetRect(realPosition);
-}
-
-//==========================================================================================	
-void TestWin::Close() {
-	realPosition = GetRect();
-	TopWindow::Close();
-}
-		
-//==========================================================================================	
-/*virtual*/ void TestWin::Xmlize(XmlIO xml) {
-	int x, y, cx, cy;
-	
-	if(xml.IsLoading()) {
-		xml("x", x);
-		xml("y", y);
-		xml("cx", cx);
-		xml("cy", cy);
-		
-		if (cy < 400) cy = 400;
-		if (cx < 400) cx = 400;
-		SetRect(x, y, cx, cy);
-		realPosition = Rect(x,y,cx,cy);
-		
-	} else {
-		// Storing
-		Rect r = GetRect();
-		xml("x", r.left);
-		xml("y", r.top);
-		xml("cx", r.right);
-		xml("cy", r.bottom);
-	}
 }
