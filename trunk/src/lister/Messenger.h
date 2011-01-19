7639e36147a58721f1482a1e7c1dd6896d448ce9
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 17:47:01 +0000

    - Placeholder for idea.  Would like tool to notify me by phone text msg when long running jobs succeed/fail

---
+++ trunk/src/lister/Messenger.h
@@ -0,0 +1,20 @@
+#ifndef _lister_lister_Messenger_h_
+#define _lister_lister_Messenger_h_
+
+/*
+Send and receive numeric- and alphanumeric text SMS messages 
+Verify delivery of outgoing SMS messages 
+Support for multimedia SMS messages, including ringtones, pictures and logo's 
+Support for WAP Push, WAP Bookmarks, vCards, voicemail/e-mail/fax/MMS indications 
+Support for Unicode, to support foreign languages like Arabic, Chinese, Hebrew, etc. 
+Support for multi-part messages, to allow messages longer than 160 characters 
+Support for GSM modems, GSM phones, SMS/HTTP providers, SMPP (Short Message Peer to Peer) providers, TAP/XIO and UCP dial-in SMSC providers 
+Support Multi-threading environments. The component is thread-safe, which means it can be used in a multi-threaded environment 
+Samples included for various development platforms: MS Visual Basic, MS Visual Basic .NET, MS Visual C++, MS Visual Studio C# .NET, ASP, ASP .NET, Borland Delphi, Borland C++ Builder, Windows Powershell ColdFusion and more 
+*/
+
+// http://www.activexperts.com/xmstoolkit/howto/mm7/html/
+
+// http://www.csoft.co.uk/sms/api/soap_to_sms.htm
+
+#endif
