f32fe4828cd9d90e58d4582203afd64ab47ad16b
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 17:27:01 +0000

    - Attempt (first) to deal with complexity of U++'s coloring/styling by bringing all known examples of Display overrides into one place for both ArrayCtrl and GridCtrl objects. - Included note on how to use

--- src/Urp/UrpPaint.h
+++ src/Urp/UrpPaint.h
@@ -3,10 +3,44 @@
 
 #include "UrpShared.h"
 
+/*
+
+grid.SetDisplay(Single<DispPM>()
+or
+AddColumn(..).SetDisplay
+
+*/
 //==============================================================================================
 struct UrpFxdWdthFntDisplay : GridDisplay {
 	virtual void Paint(Draw &w, int x, int y, int cx, int cy, const Value &val, dword style,
 			           Color &fg, Color &bg, Font &fnt, bool found, int fs, int fe);
+};
+
+//==============================================================================================
+struct TightFontDisplayForGridCtrl : GridDisplay {
+	virtual void Paint(Draw &w, int x, int y, int cx, int cy, const Value &val, dword style,
+			           Color &fg, Color &bg, Font &fnt, bool found, int fs, int fe);
+};
+
+//==============================================================================================
+struct TightFontDisplayForArrayCtrl : Display {
+	virtual void Paint(Draw& w, const Rect& r, const Value& q,
+		               Color ink, Color paper, dword style) const;
+};
+
+//==============================================================================================
+struct DispPM : GridDisplay {
+	virtual void Paint(Draw &w, int x, int y, int cx, int cy, const Value &val, dword style,
+			           Color &fg, Color &bg, Font &fnt, bool found, int fs, int fe);
+};
+
+class NumbersOnRed : public Display
+{
+public:
+  static bool numberString(String s);
+	void PaintBackground(Draw& w, const Rect& r, const Value& q,
+	                     Color ink, Color paper, dword style) const;
+	
 };
 
 #endif
