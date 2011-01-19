186eea088f990cd725b1e721c059b429f54fcfb8
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 18:10:32 +0000

    - Fix for INCREDIBLE U++ BUGGGGGG where default font goes wonky due to know know reason or change in code and remains permanently in exe and subsequent compiles, destroying readability of all text objects. - Note that this causes UrpGrid bug in column sizing, fix placed there to manage

--- trunk/src/lister/main.cpp
+++ trunk/src/lister/main.cpp
@@ -2,6 +2,11 @@
 
 GUI_APP_MAIN
 {
+	//Draw::SetStdFont(Font(Font::COURIER, 9));
+	//Draw::SetStdFont(Font(Font::ROMAN, 9));
+	//Draw::SetStdFont(Font(Font::SERIF, 11));
+	Draw::SetStdFont(Font(Font::ARIAL, 11));
+	
 	// Construct first.  Some serialization/xmilization doesn't take affect when in constructor. 
 	// (i.e., grid column widths)
 	
