7c2ba5813f24284531c1d62e11f88793783f7cff
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 18:05:32 +0000

    - Added massive support for outputing to tables, separated 2 types of table population, either Prepare and execute or copy script and shell to psql - Separated screengrid output as a function - Added row counting - Added timing - Added logging

--- trunk/src/lister/CursorHandler.h
+++ trunk/src/lister/CursorHandler.h
@@ -2,20 +2,28 @@
 #define _CursorHandler_h_
 
 #include "shared.h"
+#include "Script.h"
+
 class Connection;
 class OutputGrid;
-//==========================================================================================	
+//==============================================================================================
 class CursorHandler : public TopWindow {
 public:
-	OutputGrid *outputGrid;
 	Connection *controlConnection; // Our metadata about the query we will be running
 	Connection *connection; // Our work connection, for fetching user output
-
 	Vector<int> cw; // Column width calculator.  We add in heading widths and value widths as we load the data
-	
+
+protected:
+	// Internal support functions for the Run function
+	void RebuildTableFromConnection(String outputTable, bool log = false);
+	int LoadIntoTableFromConnectionPREP(String outputTable, int rowLimit, bool log = false);
+	int LoadIntoTableFromConnectionCOPY(String outputTable, int rowLimit, bool log = false);
+	int LoadIntoScreenGridFromConnection(OutputGrid *outputGrid, bool log = false);
+
+public:
 	CursorHandler(Connection *pcontrolConnection, Connection *pconnection);
-	void ColSize(Sql *cursor);
-	bool Run(OutputGrid *poutputGrid, String sql, bool loadIntoTable = false);
+	void ColSize(OutputGrid *outputGrid, Sql *cursor);
+	bool Run(Script &sob, bool log = false);
 };
 
 #endif
