db8ba640ac1db1c1852cc03432128ae391ab5d91
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 17:24:27 +0000

    - Added include to its own header! - demagiked config file names - Tried to deal with complexity of MS Windows behavior around load/run/execute/open/close/hide and its variation between the main window and subsequent windows.  Not finished - Dropped BINARY SERIALIZATION!  Finally.  Thing sucks for maintenance. - Started support for configging child windows, in UrpChild, testing for ContactGrid, but busted

--- src/Urp/UrpMain.h
+++ src/Urp/UrpMain.h
@@ -1,6 +1,12 @@
 #ifndef _Urp_UrpMain_h_
 #define _Urp_UrpMain_h_
 
+#define CHILDWINDOWREQSTATE_OPEN    1
+#define CHILDWINDOWREQSTATE_CLOSE   2
+#define CHILDWINDOWREQSTATE_LOAD    3
+#define CHILDWINDOWREQSTATE_SAVE    4
+
 int UrpMain(UrpTopWindow &ctrlMain, const char *mainFilePath);
+void UrpChild(UrpTopWindow &ctrlMain, UrpWindow &ctrlChild, String tagName, int requestedState);
 
 #endif
