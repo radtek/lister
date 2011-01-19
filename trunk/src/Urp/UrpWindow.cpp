b85c47f65d7bad75fe742f2ee6a652b5aa49daed
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 17:38:38 +0000

    - Added some thoughtful helpers.  For windows created dynamically, we support a map to controls attached to this window, so they don't get lost in space. - Need to zap out the SerializePlacement since not used anymore. - Woohoo! Xmlize now positions windows properly - Created vector for dynamic window creation.  Works, but positioning is way off for UrpChild - CloseIfNotClosed, OpenIfNotOpen are cool functions I've always wanted.  Why does Open crash if its open??

--- trunk/src/Urp/UrpWindow.cpp
+++ trunk/src/Urp/UrpWindow.cpp
@@ -1,12 +1,61 @@
 #include "UrpWindow.h"
+#include "UrpMain.h"
 
+//==============================================================================================
 UrpWindow::UrpWindow() : TopWindow() {
 }
 
+//==============================================================================================
 /*virtual*/ void UrpWindow::Run(bool appmodal/* = false*/) {};
 
+//==============================================================================================
+// Serialize (binary files) placement ONLY.  To be moved to Xmlize to improve maintainability
+// of configuration/customizations	
+void UrpWindow::Serialize(Stream& s) {
+	SerializePlacement(s);
+}
+
+//==============================================================================================
+void UrpWindow::AddCtrl(Ctrl *ctrl) {
+	ctrls.Add(ctrl);
+	Add(*ctrl);
+}
+
+//==============================================================================================
+/*virtual*/ void UrpWindow::Open() {
+	TopWindow::Open();
+	//SetRect(realPosition);
+};
+
+//==============================================================================================
+/*virtual*/ bool UrpWindow::OpenIfNotOpen() {
+	if (!IsOpen()) {
+		Open();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//==============================================================================================
+/*virtual*/ bool UrpWindow::CloseIfNotClosed() {
+	if (IsOpen()) {
+		Close();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//==============================================================================================
+/*virtual*/ void UrpWindow::Close() {
+	//realPosition = GetRect();
+	TopWindow::Close();
+};
+
+//==============================================================================================
 void UrpWindow::SerializePlacement(Stream& s, bool reminimize/* = false*/) {
-	
+	return;
 	GuiLock __;
 #ifndef PLATFORM_WINCE
 	int version = 0;
@@ -75,10 +124,100 @@ void UrpWindow::SerializePlacement(Stream& s, bool reminimize/* = false*/) {
 #endif
 }
 
+//==============================================================================================
+/*virtual*/ void UrpWindow::Xmlize(XmlIO xml) {
+	int x, y, cx, cy;
+	
+	if(xml.IsLoading()) {
+		xml("x", x);
+		xml("y", y);
+		xml("cx", cx);
+		xml("cy", cy);
+		
+		if (cy < 400) cy = 400;
+		if (cx < 400) cx = 400;
+		SetRect(x, y, cx, cy); // SetPos(PosLeft(x, cx), PosTop(y, cy));
+		
+		realPosition = Rect(x,y,cx,cy);
+		
+	} else {
+		// Storing
+		Rect r = GetRect();
+		x = r.left;
+		y = r.top;
+		cx = r.Width();  // Now these values do not match what was set in SetRect during the load, but the screen position comes up correctly
+		cy = r.Height();
+		xml("x", x);
+		xml("y", y);
+		xml("cx", cx);
+		xml("cy", cy);
+	}
+}
+
+//==============================================================================================
 UrpTopWindow::UrpTopWindow() : UrpWindow() {
 }
 
+//==============================================================================================
 // This must be present to extend the virtuality up to client classes, else a Run in the client 
 // class would be ignored from a call to an abstracted class.
 /*virtual*/ void UrpTopWindow::Run(bool appmodal/* = false*/) {};
+
+//==============================================================================================
+// This is the only way to do this in U++.  Failed to embed in class.
+/*static*/ VectorMap<String, UrpConfigWindow *>& UrpWindowFactory::Windows() {
+	static VectorMap<String, UrpConfigWindow *> windows;
+	return windows;
+}
+
+//==============================================================================================
+// Can't force constructor to create link to top window since usually concrete instance in main window.
+UrpWindowFactory::UrpWindowFactory() {
+}
+
+//==============================================================================================
+UrpWindowFactory::~UrpWindowFactory() {
+	for (int i = 0; i < Windows().GetCount(); i++) {
+		UrpConfigWindow *window = Windows()[i];
+		for (int j = 0; j < window->ctrls.GetCount(); j++) {
+			Ctrl * c = window->ctrls[j];
+			delete c;
+		}
+		delete window;
+	}
+}
+
+//==============================================================================================
+UrpConfigWindow * UrpWindowFactory::Open(UrpTopWindow *ptopWindow, String pwindowName) {
+	// Use this clumsy method of Get/GetAdd to avoid creating a new window on the heap that just 
+	// floats off into space when a match is found on the stack
+	UrpConfigWindow *window = Windows().Get(pwindowName, (UrpConfigWindow *)NULL);
+	if (!window) {
+		window = Windows().GetAdd(pwindowName, new UrpConfigWindow(ptopWindow, pwindowName));
+	}
+	window->Sizeable().Zoomable();
+	return window;
+}
+
+//==============================================================================================
+UrpConfigWindow::UrpConfigWindow(UrpTopWindow *ptopWindow, String pconfigName) {
+	topWindow = ptopWindow;
+	configName = pconfigName;
+}
+
+//==============================================================================================
+void UrpConfigWindow::Open() {
+	// Attach close method
+	//CtrlLayoutOKCancel(this, configName);
+	// Load config, but don't let UrpChild try to Open the window or it will recurse
+	UrpChild(*topWindow, *((UrpWindow *)this), configName, CHILDWINDOWREQSTATE_LOAD);
+	UrpWindow::Open();
+}
+
+//==============================================================================================
+void UrpConfigWindow::Close() {
+	// Save config (don't let UrpChild call Close or it will recurse)
+	UrpChild(*topWindow, *((UrpWindow *)this), configName, CHILDWINDOWREQSTATE_SAVE);
+	UrpWindow::Close();
+}
 
