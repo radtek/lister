4321175d0399e6f82e82c5885bc97d883d103a71
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 17:52:14 +0000

    - Need to ALWAYS include shared_layout to avoid failure of layout

--- src/lister/TestWin.h
+++ src/lister/TestWin.h
@@ -2,19 +2,13 @@
 #define _lister_TestWin_h_
 
 #include "shared.h"
+#include "shared_layout.h"
 
 class TestWin : public WithTestWinLayout<UrpWindow> {
 public:
 	typedef TestWin CLASSNAME;
-	Rect realPosition;
 	
 	TestWin();
-	
-	void Open();
-	
-	void Close();
-			
-	virtual void Xmlize(XmlIO xml);
 };
 
 #endif
