abc0a2c1d98277bf80c945f794f169bfb2265545
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-19 17:49:10 +0000

    - Added tons of functionality as script becomes a central manager for tasks and their execution

--- trunk/src/lister/ScriptGrid.h
+++ trunk/src/lister/ScriptGrid.h
@@ -3,17 +3,35 @@
 
 #include <lister/Urp/Urp.h>
 class Connection;
+#include "Script.h"
 
 class ScriptGrid : public UrpSqlGrid {
 public:
-	EditString taskName, scriptName, taskNote, scriptNote, scriptBody, why;
+	EditString taskName, scriptName, taskNote, scriptNote, scriptBody, why, targetName;
+	EditInt rowLimit, processOrder;
+	DropGrid connectionList, scriptTargetList, fastFlushTargetList;
+	Connection * connection;
 
+	typedef ScriptGrid CLASSNAME;
+	
 	ScriptGrid();
-	void Build();
-	void Load(Connection *pconnection);
+	void Build(Connection *pconnection);
+	void Load();
+	void LoadScriptTargetList(DropGrid &pscriptTargetList);
+	void PrepTargetFastFlush(DropGrid &fastFlushTargetList);
 	void LinkScriptToTaskGrid(ArrayCtrl &taskGrid);
+	// Always construct a script object with an output grid pointer (or null)
+	void GetScriptOb(int row, Script *psob); 
 	int GetScriptId(int row);
+	int GetConnId(int row);
+	int GetRowLimit(int row);
+	int GetProcessOrder(int row);
 	String GetScript(int row);
+	ScriptTarget GetScriptTarget(int row);
+	String GetTargetName(int row);
+	int GetRelId(int row);
+	String GetWhy(int row);
+	bool GetFastFlushTarget(int row);
 };
 
 #endif
