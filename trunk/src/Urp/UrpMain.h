630bd91c48a72edd20d148e080a940e9ecbb2d9d
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-01-26 22:45:48 +0000

    - Link to top window, path can't be passed in the constructor.

--- trunk/src/Urp/UrpMain.h
+++ trunk/src/Urp/UrpMain.h
@@ -1,12 +1,15 @@
 #ifndef _Urp_UrpMain_h_
 #define _Urp_UrpMain_h_
 
+#include "UrpTopWindow.h"
+#include "UrpWindow.h"
+
 #define CHILDWINDOWREQSTATE_OPEN    1
 #define CHILDWINDOWREQSTATE_CLOSE   2
 #define CHILDWINDOWREQSTATE_LOAD    3
 #define CHILDWINDOWREQSTATE_SAVE    4
 
-int UrpMain(UrpTopWindow &ctrlMain, const char *mainFilePath);
+int UrpMain(UrpTopWindow &ctrlMain);
 void UrpChild(UrpTopWindow &ctrlMain, UrpWindow &ctrlChild, String tagName, int requestedState);
 
 #endif
