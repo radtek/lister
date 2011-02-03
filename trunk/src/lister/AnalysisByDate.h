cd34f4f490c8b1032ec9876dbf5ec8c7940fe90e
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-02-03 16:20:52 +0000

    - Started plan to track analysis efforts that are by business date.

---
+++ trunk/src/lister/AnalysisByDate.h
@@ -0,0 +1,23 @@
+#ifndef _lister_lister_AnalysisByDate_h_
+#define _lister_lister_AnalysisByDate_h_
+
+#include "shared.h"
+#include "shared_db.h"
+class Connection;
+
+class AnalysisByDateGrid : public UrpSqlGrid {
+public:
+	typedef AnalysisByDateGrid CLASSNAME;
+	
+	int            analysisId, scriptId;
+	EditString     analysisName, totalCount, note;
+	Connection    *connection; // Our metadata about the query we will be running
+	
+	               AnalysisByDateGrid();
+	void           UpdatingRow();
+	void           Build(Connection *pconnection);
+	void           Load();
+	void           SetTaskId(int ptaskId);
+};
+
+#endif
