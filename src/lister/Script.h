f1007bdd43ceba70db05aa4be0138537365c1537
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-02-03 17:47:35 +0000

    - No real change

--- src/lister/Script.h
+++ src/lister/Script.h
@@ -4,11 +4,19 @@
 #include "shared.h"
 
 class OutputGrid;
+class OutputStat;
 
-enum ScriptTarget { SO_UNDEF, SO_SCREEN, SO_TABLE };
+// These can't change; stuffed in database.
+
 
 class Script : public Moveable<Script> { // MoveableAndDeepCopyOption?? See Vcont.h for sample
 public:
+	enum ScriptTarget { 
+			SO_UNDEF
+		,	SO_SCREEN
+		,	SO_TABLE 
+	};
+	
 	int                     scriptId // key from scripts table
 	,                       originalScriptId; // Should populate so original script can bne updated when shift key is held down
 	ScriptTarget            scriptTarget; // Screen or table?
