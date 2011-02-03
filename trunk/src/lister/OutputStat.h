b8a4114ef1369cd08d4e7d7a13110d2c78ab4008
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-02-03 18:01:31 +0000

    - Track status visually for script runs.  Several attributes tracked.

--- trunk/src/lister/OutputStat.h
+++ trunk/src/lister/OutputStat.h
@@ -2,11 +2,29 @@
 #define _lister_lister_OutputStat_h_
 
 #include "shared.h"
-#include "shared_layout.h"
+#include "lister/Urp/UrpInterval.h"
 
-class OutputStat : public WithOutputStatLayout<UrpWindow> {
+#define LAYOUTFILE <lister/lister/panels.lay>
+#include <CtrlCore/lay.h>
+
+class OutputStat : public WithOutputStatLayout<ParentCtrl> { // Note: Not a TopWindow type so it can be embedded
 public:
-	OutputStat();
+	typedef OutputStat CLASSNAME;
+	Time   *startedWhen;
+	        OutputStat();
+	       ~OutputStat();
+	String  PrepOutputValue(String in);
+	// User calls these wrappers on completion of execution of a cursor/script and doesn't have
+	// to know if this is display or write to a log or db, etc.
+	void    SetRowCount(int prowCount);
+	void    SetColumnCount(int pcolumnCount);
+	void    SetConnName(String pconnName);
+	void    SetFetchTime(Interval ptime);
+	void    SetScriptId(int id);
+	void    SetStartedWhen(Time pstartedWhen);
+	Time    GetStartedWhen();
+	void    SetStoppedWhen(Time pstoppedWhen);
+	void    SetStatus(String pstatus);
 };
 
 #endif
