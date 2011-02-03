0574a09b8d5beb7e7288ff2420d6104f59eb1e37
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-02-03 16:26:02 +0000

    - Add outputstatus handle to job spec so it gets passed around

--- src/lister/JobSpec.h
+++ src/lister/JobSpec.h
@@ -2,15 +2,17 @@
 #define _lister_lister_JobSpec_h_
 
 #include "shared.h"
+class OutputStat;
 
 class JobSpec {
 public:
-	bool     log;
-	bool     batchMode; // No popups
-	bool     testMode; // 100 rows each
-	bool     executingSelection;
+	bool         log;
+	bool         batchMode; // No popups
+	bool         testMode; // 100 rows each
+	bool         executingSelection;
+	OutputStat  *outputStat;
 	
-	         JobSpec(bool plog = true, bool ptestMode = true, bool pbatchMode = false);
+	             JobSpec(bool plog = true, bool ptestMode = true, bool pbatchMode = false, OutputStat *poutputStat = NULL);
 };
 
 #endif
