bcc7d74c6164217b985470cd34b7ebf1241f8dcd
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-02-08 21:49:26 +0000

    - Add method to get InstanceName

--- trunk/src/lister/NewInstanceWin.h
+++ trunk/src/lister/NewInstanceWin.h
@@ -1,14 +1,15 @@
 #ifndef _NewInstanceWin_h_
 #define _NewInstanceWin_h_
 
-//#include "shared.h"
+#include "shared.h"
 #include "shared_layout.h"
 
 class NewInstanceWin : public WithNewInstanceLayout<TopWindow> {
 public:
 	typedef NewInstanceWin CLASSNAME;
 
-	NewInstanceWin();
+	       NewInstanceWin();
+	String GetInstanceName();
 };
 
 #endif
