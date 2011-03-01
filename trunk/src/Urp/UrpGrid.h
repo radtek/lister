295b6a74da454d943b9b83cc88a3577b121f69aa
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-01 23:53:44 +0000

    - Cleanup - Create GetFirstSelection to simulate function from ArrayCtrl - Restore last selected row as we did in UrpSqlGrid

--- trunk/src/Urp/UrpGrid.h
+++ trunk/src/Urp/UrpGrid.h
@@ -7,27 +7,29 @@
 #include <lister/GridCtrl/GridCtrl.h>
 
 #define HIDDEN_COLUMN -2
+#define NOSELECTION   -4
 
 //==========================================================================================	
 class UrpGrid: public GridCtrl, public UrpGridCommon {
 public:
-	UrpGrid();
-	virtual bool Key(dword key, int count) { return GridCtrl::Key(key, count); };
-	// Obviously, U++ developer Uno was being a total dickwad when he refused to give a function
-	// to extract column widths, so I've created it.
-	int GetFloatingColumnWidth(int colno);
-	// Series of corrective functions to deal with Lamo Uno's confusion about when and when not to adjust references for fixed columns
-	int GetFloatingColumnCount();
-	void UnhideFloatingColumnSilently(int i);
-	void HideFloatingColumn(int i);
-	void SetFloatingColumnWidth(int i, int w);
-	Id GetFloatingColumnId(int n) const;
+	typedef UrpGrid CLASSNAME;
+	                    UrpGrid();
+	virtual bool        Key(dword key, int count) { return GridCtrl::Key(key, count); };
+	                    // Obviously, U++ developer Uno was being a total dickwad when he refused to give a function
+	                    // to extract column widths, so I've created it.
+	int                 GetFloatingColumnWidth(int colno);
+	                    // Series of corrective functions to deal with Lamo Uno's confusion about when and when not to adjust references for fixed columns
+	int                 GetFloatingColumnCount();
+	void                UnhideFloatingColumnSilently(int i);
+	void                HideFloatingColumn(int i);
+	void                SetFloatingColumnWidth(int i, int w);
+	Id                  GetFloatingColumnId(int n) const;
 	GridCtrl::ItemRect& GetFloatingColumn(int n);
-	
-	//  GridCtrl is supposed to Xmlize, but I don't see it doing anything, so I've written my own.
-	//  Have to save by name so that code changes that add/subtract columns will not cause confusion.
-	//  Also, we save hidden state by name instead of position, which can really mess with a grid.
-	void Xmlize(XmlIO xml);
+	int                 GetFirstSelection();
+	                    // GridCtrl is supposed to Xmlize, but I don't see it doing anything, so I've written my own.
+	                    // Have to save by name so that code changes that add/subtract columns will not cause confusion.
+	                    // Also, we save hidden state by name instead of position, which can really mess with a grid.
+	void                Xmlize(XmlIO xml);
 	
 };
 
