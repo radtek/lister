b3ec9494c40c76607ebfaee17d1458a9131ee4de
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-01 15:55:57 +0000

    - Added IsOSAuth function, but not quite working yet - Added Setters to support new instance creation - Prettied up spaceing

--- trunk/src/lister/ConnGrid.h
+++ trunk/src/lister/ConnGrid.h
@@ -8,72 +8,68 @@
 #include "NewInstanceWin.h"
 #include "ConnectedCtrl.h"
 
-//==========================================================================================	
+//==============================================================================================
 class ConnGrid : public UrpGrid, ConnectedCtrl {
 public:
 	
-	EditInt fldConnId;
-	EditStringNotNull fldConnName;               // 0) A meaningful name; too many connections to track without a name
-	EditStringNotNull fldLoginStr;               // 1) The login string
-	EditString fldLoginPwd;                      // 2) The password in plain text
-	EditString fldInstNm;                // 3) The server connecting to, to display to user
-	EditString fldInstanceAddress; // 4) The actual address code to pass to the connector function
-	EditStringNotNull fldInstTypName;          // 6) String form of type of server: Oracle, MS SQL, etc, which connector we use
-	EditString fldEnvStdName;                // 7) Environment: Dev, UAT, prod.  This is the color
-	EditString fldConnNote;
-	EditString fldDbName;	
-	DropGrid instTypList;
-	DropGrid instanceList;
-	DropGrid envList;
-	
-	NewInstanceWin newInstanceWin;
+	EditInt           fldConnId;
+	EditStringNotNull fldConnName;          // 0) A meaningful name; too many connections to track without a name
+	EditStringNotNull fldLoginStr;          // 1) The login string
+	EditString        fldLoginPwd;          // 2) The password in plain text
+	EditString        fldInstNm;            // 3) The server connecting to, to display to user
+	EditString        fldInstanceAddress;   // 4) The actual address code to pass to the connector function
+	EditStringNotNull fldInstTypName;       // 6) String form of type of server: Oracle, MS SQL, etc, which connector we use
+	EditString        fldEnvStdName;        // 7) Environment: Dev, UAT, prod.  This is the color
+	EditString        fldConnNote;
+	EditString        fldDbName;	
+	DropGrid          instTypList;
+	DropGrid          instanceList;
+	DropGrid          envList;
+	DropGrid          osAuthList;
+	NewInstanceWin    newInstanceWin;
 		
 	typedef ConnGrid CLASSNAME;
 	
 	// Everything you need to connect to a server
 	//GridCtrl *outputGrid;
 
-	ConnGrid();
-	int GetConnId(int row);
-	void SetConnId(int row, int pconnId);
-	String GetConnName(int row);
-	String GetInstanceTypeName(int row);
-	int GetInstanceId(int row);
-	void SetInstanceId(int row, int pinstId);
-	String GetInstanceName(int row);
-	String GetInstanceAddress(int row);
-	int GetInstTypId(int row);
-	String GetDatabaseName(int row);
-	int GetEnvId(int row);
-	int GetLoginId(int row);
-	String GetLoginStr(int row);
-	String GetLoginPwd(int row);
-	ConnState *ConnGrid::GetConnState(int row);
-
-	void NewConn();
-	
-	void Build();
-	
+	                  ConnGrid              ();
+	int               GetConnId             (int row);
+	void              SetConnId             (int row, int pconnId);
+	String            GetConnName           (int row);
+	String            GetInstanceTypeName   (int row);
+	int               GetInstanceId         (int row);
+	void              SetInstanceId         (int row, int pinstId);
+	String            GetInstanceName       (int row);
+	void              SetInstanceName       (int row, String pinstNm);
+	String            GetInstanceAddress    (int row);
+	void              SetInstanceAddress (int row, String pinstAdr);
+	int               GetInstTypId          (int row);
+	void              SetInstTypId          (int row, int pinstTypId);
+	String            GetDatabaseName       (int row);
+	int               GetEnvId              (int row);
+	void              SetEnvId              (int row, int penvId);
+	int               GetLoginId            (int row);
+	String            GetLoginStr           (int row);
+	String            GetLoginPwd           (int row);
+	ConnState *       GetConnState          (int row);
+	void              SetConnState          (int row, EnumConnState enumConnState);
+	bool              GetOSAuth             (int row);
+	void              NewConn               ();
+	void              Build                 ();
 	// Popup to let user enter new connection instance details
-	void NewInstance();
-
+	void              NewInstance           ();
 	// Has to pass a connection that persists after this window closes
-	virtual void Load(Connection *pconnection);
-	
+	virtual void      Load                  (Connection *pconnection);
 	//  Let main interface set colors representing the state of the connection.
-	void SetConnState(int row, EnumConnState enumConnState);
-	
-	void AddedNewConnection();
-		
+	void              AddedNewConnection    ();
 	// Don't care if the connect button changed, or the color changed.
-	bool MeaningfulDataChange();
-
+	bool              MeaningfulDataChange  ();
 	//  The main interface calls this to position and select a specific connection, usually
 	//  so it can trigger a connect event.
-	bool FindConnName(String pconnName, bool silent = false);
-	bool FindConnId(int pconnId, bool silent = false );
-
-	bool WasConnectionRequested();
+	bool              FindConnName          (String pconnName, bool silent = false);
+	bool              FindConnId            (int pconnId, bool silent = false );
+	bool              WasConnectionRequested();
 };
 
 #endif
