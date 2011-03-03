c4f39d94145a3ccd6ef1ec06639a4f2be5db255d
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-03 04:11:48 +0000

    - Prettify

--- src/lister/ScriptGrid.h
+++ src/lister/ScriptGrid.h
@@ -10,31 +10,34 @@ class ScriptGrid : public UrpSqlGrid {
 public:
 	typedef ScriptGrid CLASSNAME;
 
-	EditString      taskName, scriptName, taskNote, scriptNote, scriptBody, why, targetName;
-	EditInt         rowLimit, processOrder;
-	DropGrid        connectionList, scriptTargetList, fastFlushTargetList;
-	Connection     *connection;
+	EditString           taskName, scriptName, taskNote, scriptNote, scriptBody, why, targetName;
+	EditInt              rowLimit, processOrder;
+	DropGrid             connectionList, scriptTargetList, fastFlushTargetList;
+	Connection          *connection;
 	
- 	                ScriptGrid();
-	void            Build(Connection *pconnection);
-	void            Load();
-	void            LoadScriptTargetList(DropGrid &pscriptTargetList);
-	void            PrepTargetFastFlush(DropGrid &fastFlushTargetList);
-	void            LinkScriptToTaskGrid(ArrayCtrl &taskGrid);
-	// Always construct a script object with an output grid pointer (or null)
-	void            GetScriptOb(int row, Script *psob); 
-	int             GetScriptId(int row);
-	int             GetConnId(int row);
-	int             GetRowLimit(int row);
-	int             GetProcessOrder(int row);
-	int             GetMaxProcessOrder();
-	RichText        GetScriptRichText(int row);
-	String          GetScriptPlainText(int row);
-	Script::ScriptTarget    GetScriptTarget(int row);
-	String          GetTargetName(int row);
-	int             GetRelId(int row);
-	String          GetWhy(int row);
-	bool            GetFastFlushTarget(int row);
+ 	                     ScriptGrid();
+ 	void                 RebuildOrder();
+ 	void                 DroppedNewRows(int line, int howmanylines);
+	void                 Build(Connection *pconnection);
+	void                 Load();
+	void                 LoadScriptTargetList(DropGrid &pscriptTargetList);
+	void                 PrepTargetFastFlush(DropGrid &fastFlushTargetList);
+	void                 LinkScriptToTaskGrid(ArrayCtrl &taskGrid);
+	                     // Always construct a script object with an output grid pointer (or null)
+	void                 GetScriptOb(int row, Script *psob); 
+	int                  GetScriptId(int row);
+	int                  GetConnId(int row);
+	int                  GetRowLimit(int row);
+	int                  GetProcessOrder(int row);
+	int                  GetMaxProcessOrder();
+	RichText             GetScriptRichText(int row);
+	String               GetScriptPlainText(int row);
+	Script::ScriptTarget GetScriptTarget(int row);
+	String               GetTargetName(int row);
+	int                  GetRelId(int row);
+	String               GetWhy(int row);
+	bool                 GetFastFlushTarget(int row);
+	void                 UpdateTaskScriptGrid(int pscriptId, String pscript);
 };
 
 #endif
