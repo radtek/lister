36e2b572e53e4bb35c1477fbd4d20d01cdcba828
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-08 00:27:20 +0000

    - New class to add to task editor to manage macros for scripts under this task

---
+++ trunk/src/lister/TaskMacroGrid.h
@@ -0,0 +1,24 @@
+#ifndef _lister_lister_TaskMacroGrid_h_
+#define _lister_lister_TaskMacroGrid_h_
+
+#include "shared.h"
+#include "shared_db.h"
+class Connection;
+
+class TaskMacroGrid : public UrpSqlGrid {
+public:
+	typedef TaskMacroGrid CLASSNAME;
+	
+	int            taskId;
+	EditString     searchFor, replaceWith, expansion, note;
+	Connection    *connection; // Our metadata about the query we will be running
+	
+	               TaskMacroGrid();
+	void           UpdatingRow();
+	void           Build(Connection *pconnection);
+	void           Load();
+	void           SetTaskId(int ptaskId);
+	static void    LoadTaskMacro(Connection *connection, DropGrid &dropGrid);
+};
+
+#endif
