fc65a399a29fe686d2fa3eadcfdd98acd65ee429
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-08 00:08:09 +0000

    - Created ContextMacros class to support building the macro dropdown when the task changed

---
+++ trunk/src/lister/ContextMacros.cpp
@@ -0,0 +1,14 @@
+#include "ContextMacros.h"
+
+void ContextMacros::UpdateAvailableMacros(DropGrid &macrosAvailableList, ContextMacros *activeContextMacros) {
+	
+	macrosAvailableList.Clear();
+	if (!activeContextMacros) return;
+	MacMap &taskMacros = activeContextMacros->taskMacros;
+	for (int i = 0; i < taskMacros.GetCount(); i++) {
+		MacPair macPair = taskMacros.Get(taskMacros.GetKey(i));
+		macrosAvailableList.Add(taskMacros.GetKey(i), macPair.replaceWith, macPair.expansion);
+	}
+
+}
+
