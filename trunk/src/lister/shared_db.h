91bffab826fb8ca29f7984c605a307ec39d1a144
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-08 04:19:28 +0000

    - Fixed bad include to non-customized Postgre

--- trunk/src/lister/shared_db.h
+++ trunk/src/lister/shared_db.h
@@ -1,12 +1,12 @@
 #ifndef _lister_lister_shared_db_h_
 #define _lister_lister_shared_db_h_
 
-#include <PostgreSQL/PostgreSQL.h>
+#include <lister/PostgreSQL/PostgreSQL.h>
 
-#define SCHEMADIALECT <PostgreSQL/PostgreSQLSchema.h>
+#define SCHEMADIALECT <lister/PostgreSQL/PostgreSQLSchema.h>
 #define MODEL <lister/lister/database.sch>
 
-#include "Sql/sch_header.h"
+#include "lister/Sql/sch_header.h"
 //#include <lister/Sql/sch_schema.h>
 
 #endif
