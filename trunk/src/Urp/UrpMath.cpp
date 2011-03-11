54ae793a7b19f012d38d67dfe05c26636b4cf067
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-11 21:22:43 +0000

    - Moved Sgn from UrpShared to here - Added Abs

---
+++ trunk/src/Urp/UrpMath.cpp
@@ -0,0 +1,10 @@
+#include "UrpMath.h"
+
+int Sgn(int i) {
+	return (i >= 0? 1 : -1);
+}
+
+unsigned int Abs(int i) {
+	return abs(i);
+}
+
---
+++ trunk/src/Urp/UrpMath.h
@@ -0,0 +1,9 @@
+#ifndef _lister_Urp_UrpMath_h_
+#define _lister_Urp_UrpMath_h_
+
+#include "UrpShared.h"
+
+int Sgn(int i);
+unsigned int Abs(int i);
+
+#endif
