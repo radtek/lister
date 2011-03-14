446824b583914a34eefcb592af70a640f6b87e5a
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-14 20:54:32 +0000

    - Ehanced to use our CalcCorrectRow to fetch the actual active row

--- src/lister/ScriptGrid.cpp
+++ src/lister/ScriptGrid.cpp
@@ -103,12 +103,12 @@ void ScriptGrid::LinkScriptToTaskGrid(ArrayCtrl &taskGrid) {
 
 //==============================================================================================
 int ScriptGrid::GetRowLimit(int row) {
-	return Upp::max((int)Get(row, ROWLIMIT), (int)-1); // -1 in this case indicates no limit
+	return Upp::max((int)Get(CalcCorrectRow(row), ROWLIMIT), (int)-1); // -1 in this case indicates no limit
 }
 
 //==============================================================================================
 int ScriptGrid::GetProcessOrder(int row) {
-	return Upp::max((int)Get(row, PROCESSORDER), (int)-1);
+	return Upp::max((int)Get(CalcCorrectRow(row), PROCESSORDER), (int)-1);
 }
 
 //==============================================================================================
@@ -127,12 +127,12 @@ int ScriptGrid::GetMaxProcessOrder() {
 
 //==============================================================================================
 int ScriptGrid::GetConnId(int row) {
-	return Upp::max((int)Get(row, RELCONNID), UNKNOWN);
+	return Upp::max((int)Get(CalcCorrectRow(row), RELCONNID), UNKNOWN);
 }
 
 //==============================================================================================
 int ScriptGrid::GetScriptId(int row) {
-	return Upp::max((int)Get(row, SCRIPTID), UNKNOWN);
+	return Upp::max((int)Get(CalcCorrectRow(row), SCRIPTID), UNKNOWN);
 }
 
 //==============================================================================================
@@ -140,11 +140,11 @@ RichText ScriptGrid::GetScriptRichText(int row) {
 	Value v;
 	
 	if (v.IsNull()) {
-		v = Get(row, SCRIPTPLAINTEXT);
+		v = Get(CalcCorrectRow(row), SCRIPTPLAINTEXT);
 		String s = v.ToString();
 		return ParseQTF(DeQtfLf(s));
 	} else {
-		v = Get(row, SCRIPTRICHTEXT);
+		v = Get(CalcCorrectRow(row), SCRIPTRICHTEXT);
 		return AsRichText(WString(v));
 	}
 }
@@ -157,43 +157,43 @@ String ScriptGrid::GetScriptPlainText(int row) {
 //==============================================================================================
 void ScriptGrid::GetScriptOb(int row, Script *psob) {
 	ASSERT(psob);
-	psob->scriptPlainText = GetScriptPlainText  (row);
-	psob->scriptRichText  = GetScriptRichText   (row);
-	psob->scriptId        = GetScriptId         (row);
-	psob->scriptTarget    = GetScriptTarget     (row);
-	psob->targetName      = GetTargetName       (row);
-	psob->fastFlushTarget = GetFastFlushTarget  (row);
-	psob->rowLimit        = GetRowLimit         (row); // Important for testing
-	psob->processOrder    = GetProcessOrder     (row);
+	psob->scriptPlainText = GetScriptPlainText  (CalcCorrectRow(row));
+	psob->scriptRichText  = GetScriptRichText   (CalcCorrectRow(row));
+	psob->scriptId        = GetScriptId         (CalcCorrectRow(row));
+	psob->scriptTarget    = GetScriptTarget     (CalcCorrectRow(row));
+	psob->targetName      = GetTargetName       (CalcCorrectRow(row));
+	psob->fastFlushTarget = GetFastFlushTarget  (CalcCorrectRow(row));
+	psob->rowLimit        = GetRowLimit         (CalcCorrectRow(row)); // Important for testing
+	psob->processOrder    = GetProcessOrder     (CalcCorrectRow(row));
 }
 
 //==============================================================================================
 Script::ScriptTarget ScriptGrid::GetScriptTarget(int row) {
-	int intScriptTarget = Get(row, SCRIPTTARGET);
+	int intScriptTarget = Get(CalcCorrectRow(row), SCRIPTTARGET);
 	Script::ScriptTarget scriptTarget = (Script::ScriptTarget)intScriptTarget;
 	return Upp::max(scriptTarget, Script::SO_UNDEF); // Put a 0 if big negative returned for a null.
 }
 
 //==============================================================================================
 String ScriptGrid::GetTargetName(int row) {
-	return Get(row, TARGETNAME);
+	return Get(CalcCorrectRow(row), TARGETNAME);
 }
 
 //==============================================================================================
 int ScriptGrid::GetRelId(int row) {
-	return Get(row, RELID);
+	return Get(CalcCorrectRow(row), RELID);
 }
 
 //==============================================================================================
 String ScriptGrid::GetWhy(int row) {
-	return Get(row, WHY);
+	return Get(CalcCorrectRow(row), WHY);
 }
 
 //==============================================================================================
 // Case for dealing with PostgreSQL booleans as strings "1" and "0" (Bug in U++ driver)
 // Convert them from strings to boolean
 bool ScriptGrid::GetFastFlushTarget(int row) {
-	String v = Get(row, FASTFLUSHTARGET);
+	String v = Get(CalcCorrectRow(row), FASTFLUSHTARGET);
 	return (v == "1");
 }
 
