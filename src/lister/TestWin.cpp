4b53f302782dd17e0c20ef7f0050ccde8d18e77a
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-17 19:59:49 +0000

    - no change

--- src/lister/TestWin.cpp
+++ src/lister/TestWin.cpp
@@ -4,5 +4,6 @@
 TestWin::TestWin() {
 	CtrlLayout(*this, "Browse All Tests");
 	Sizeable().Zoomable();
+	
 	testGrid.SizePos(); // Note: Cannot SizePos in TestGrid.Load() or Graphics will cycle onto all cells
 }
