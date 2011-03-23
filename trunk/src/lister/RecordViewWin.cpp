e8b9aa7b68f565e3c08c8d8455bbe9e08b526008
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-23 19:30:41 +0000

    - Tried (Failed) to set icon in constructor.  I guess for dynamic windows, icon is ignored.

--- trunk/src/lister/RecordViewWin.cpp
+++ trunk/src/lister/RecordViewWin.cpp
@@ -1,9 +1,16 @@
 #include "RecordViewWin.h"
 
+#include "image_shared.h"
+#include <Draw/iml_header.h>
+
+// Only in lister.cpp
+//#include "image_shared.h"
+//#include <Draw/iml_source.h>
+
 //==============================================================================================
 RecordViewWin::RecordViewWin() {
 	CtrlLayout(*this, "Record View");
 	Sizeable().Zoomable();
-	
+	// Icon ignored when set here. Icon(MyImages::recview16());
 	recordViewGrid.SizePos(); // Note: Cannot SizePos in TestGrid.Load() or Graphics will cycle onto all cells
 }
