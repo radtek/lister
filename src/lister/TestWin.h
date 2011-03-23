7861a30f36578747b0b24286ecd88044af1be952
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-23 04:18:47 +0000

    - Major expansion with Test runner here from lister. - Major functions, Build, Toolbar, RunAllTestsAsBatch, RunTest wrapper

--- src/lister/TestWin.h
+++ src/lister/TestWin.h
@@ -3,13 +3,30 @@
 
 #include "shared.h"
 #include "shared_layout.h"
-
+#include <lister/Urp/UrpGrid.h>
+class ContextMacros;
+class Connection;
 //==============================================================================================
 class TestWin : public WithTestWinLayout<UrpWindow> {
 public:
 	typedef TestWin CLASSNAME;
+	Connection    *controlConnection
+	               // Since this is passed to a thread (a window), it is a pointer to the pointer
+	               // to the activeCOnnection (?), so if it is changed from the main window,
+	               // we can hopefully pick it up.
+	,             **lastActiveConnection;
+	ToolBar        toolbar;           // Tools for user to control test processing.
+	ContextMacros *activeContextMacros; // Active for this test window.
+	Callback       WhenToolBarNeedsUpdating;
 	
-	TestWin();
+	               TestWin();
+	void           Build(Connection *pconnection, Connection **plastActiveConnection, ContextMacros *pactiveContextMacros);
+	void           MyToolBar(Bar& bar);
+	void           RunAllTestsAsBatch();
+	void           UpdateToolBar(UrpGrid::GridAction gridAction);
+	void           ClickedTest(); // GUI wrapper for RunTest function
+	String         RunTest(int row);
+	virtual void   Close();
 };
 
 #endif
