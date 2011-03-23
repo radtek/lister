fdacba1ef44d56ce8b4ab0306826b5487747e583
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-23 04:09:50 +0000

    - Major Refactor; Brought RebuildMacros from lister.cpp main prog to here so TestWin could use it.

--- src/lister/ContextMacros.cpp
+++ src/lister/ContextMacros.cpp
@@ -1,9 +1,13 @@
 #include "ContextMacros.h"
 #include "MacroHandler.h"
+#include "Connection.h"
 
+//==============================================================================================
 ContextMacros::ContextMacros() {
 	allMacros.Add(&envMacros);
 	allMacros.Add(&taskMacros);
+	rebuildTaskMacros = true;
+	taskId = UNKNOWN;
 }
 
 //==============================================================================================
@@ -11,6 +15,7 @@ void ContextMacros::UpdateAvailableMacros(DropGrid &macrosAvailableList, Context
 	
 	macrosAvailableList.Clear();
 	if (!activeContextMacros) return;
+	int macrosetcount = activeContextMacros->allMacros.GetCount();
 	
 	for (int j = 0; j < activeContextMacros->allMacros.GetCount(); j++) {
 		MacMap &macros = *(activeContextMacros->allMacros[j]);
@@ -21,5 +26,59 @@ void ContextMacros::UpdateAvailableMacros(DropGrid &macrosAvailableList, Context
 	}
 	
 	UpdateMacroList(macrosAvailableList);
+}
+
+//==============================================================================================	
+void ContextMacros::RebuildMacros(int connId, ContextMacros *activeContextMacros, Connection *controlConnection, Connection *lastActiveConnection, DropGrid *macrosAvailableList/*=NULL*/) {
+	String loadedEnvLetter;
+
+	if (connId != UNKNOWN) {		
+		Connection *connInfo = connectionFactory->FetchConnInfo(connId);
+		loadedEnvLetter = connInfo->envLetter;
+		delete connInfo;  // We created an object that is not on a stack anywhere, so we must destroy
+	} else 
+	if (lastActiveConnection) {
+		loadedEnvLetter = lastActiveConnection->envLetter;
+	} else {
+		loadedEnvLetter = "XX"; // Default to a "Not an environment" value
+	}
+
+	// Only reload environment macros if there was a change in environment
+	if (!loadedEnvLetter.IsEmpty() && loadedEnvLetter != envLetter) {
+		envLetter = loadedEnvLetter;
+		activeContextMacros->envMacros.Clear();
+		activeContextMacros->envMacros.Add("ENV", MacPair(envLetter, envLetter));
+		rebuildTaskMacros = true; // Force rebuild of task macros since they probably reference the ENV macro
+	}
+	
+	// If we've stayed within the same task set of scripts, we don't need to rebuild tasks.
+	if (rebuildTaskMacros) {
+		rebuildTaskMacros = false; // Desensitize flag
+		
+		// Flush CurrentTaskMacroList; fetch from taskmacros.  Apply logic from CursorHandler macro function as each is loaded.
+		// Move macro code out of CursorHandler and into MacroHandler.cpp
+		// Alter macro function to rerun macros in CurrentTaskMacroList, then macros in current script.
+		activeContextMacros->taskMacros.Clear();
+		
+		if (controlConnection->SendQueryDataScript(SqlStatement(SqlSelect(SEARCHFOR, REPLACEWITH).From(TASKMACROS).Where(TASKID==taskId).OrderBy(PROCESSORDER)).GetText())) {
+			while(controlConnection->Fetch()) {
+				String searchFor = controlConnection->Get(0);
+				String replaceWith = controlConnection->Get(1);
+				
+				// Expand the output with any previous macros in task list, as well as the standard hardcoded ones
+				String expansion = ExpandMacros(replaceWith, activeContextMacros);
+				
+				// Add it to the list and continue fetching and expanding
+				
+				activeContextMacros->taskMacros.Add(searchFor, MacPair(replaceWith, expansion));
+			}
+		}
+		
+		// Update the drop down so script-writer can see what macros are available
+
+		if (macrosAvailableList) {		
+			activeContextMacros->UpdateAvailableMacros(*macrosAvailableList, activeContextMacros);
+		}
+	}
 }
 
