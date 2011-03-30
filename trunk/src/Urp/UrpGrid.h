dbce15ab86ba662f0b71393645d02dae62db7a75
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-30 17:01:42 +0000

    - Added Connection shared code for Build and Load - Fixed cases where Xmlize tried to set a grid row position to a non-existent row.

--- trunk/src/Urp/UrpGrid.h
+++ trunk/src/Urp/UrpGrid.h
@@ -3,13 +3,14 @@
 
 #include "UrpShared.h"
 #include "UrpGridCommon.h"
-
 #include <lister/GridCtrl/GridCtrl.h>
+class Connection; // Urp has no connection class (yet) so lister, our primary user, defines it.
 
 #define HIDDEN_COLUMN -2
 #define NOSELECTION   -4
 
-//==========================================================================================	
+//==============================================================================================
+// abstract base class; ye canna instantiate
 class UrpGrid: public GridCtrl, public UrpGridCommon {
 public:
 	typedef UrpGrid CLASSNAME;
@@ -23,39 +24,61 @@ public:
 		DELETEDROWS 
 	};
 	
+                          // Build function of subclass should set to true, but no biggy; Its more for the subclasses.  Urp doesn't use yet or expect any settings.
+                          // TaskDefWin does look at this flag.
+	bool                  built;  
+	
+	                      // Load function of subclass should set to true; The idea is to reduce work of reloading unnecessarily, but hard to get it to work
+	bool                  loaded; 
+	
+	                      // Loose add so grids like TestGrid will call TestWin to update its
+                          // toolbar when testrows are added, columns added, etc.
+	Callback1<GridAction> WhenToolBarNeedsUpdating;
 
-	bool                  built; // Build function of subclass should set to true, but no biggy; Its more for the subclasses.  Urp doesn't use yet or expect any settings
-	bool                  loaded; // Load function of subclass should set to true; The idea is to reduce work of reloading unnecessarily, but hard to get it to work
-	Callback1<GridAction> WhenToolBarNeedsUpdating;  // Loose add so grids like TestGrid will call TestWin to update its toolbar when testrows are added, columns added, etc.
+	                      UrpGrid                     ();
+	                      
+	                      // Mandatory implementations (pure virtual functions)
+	virtual void          Build                       (Connection * pconnection) = 0;
+	virtual void          Load                        () = 0;
 
-	                    UrpGrid();
-	virtual bool        Key(dword key, int count) { return GridCtrl::Key(key, count); };
-	                    // Old windows have coked-up XML with bizarre widths.  This should help (menu item)
-	void                NormalizeColumnWidth();
-	                    // Obviously, U++ developer Uno was being a total dickwad when he refused to give a function
-	                    // to extract column widths, so I've created it.
-	                    
-	int                 GetFloatingColumnWidth(int colno);
-	                    // Series of corrective functions to deal with Lamo Uno's confusion about when and when not to adjust references for fixed columns
-	int                 GetFloatingColumnCount();
-	void                UnhideFloatingColumnSilently(int i);
-	void                HideFloatingColumn(int i);
-	void                SetFloatingColumnWidth(int i, int w);
-	Id                  GetFloatingColumnId(int n) const;
-	GridCtrl::ItemRect& GetFloatingColumn(int n);
-	int                 GetFirstSelection();
-	int                 CalcCorrectRow(int row);
-	int                 GetProcessOrder(int row = -1);
-	int                 GetMaxProcessOrder();
-	int                 GetNextProcessOrder();
-	void                MovedRows();
-	                    // Trick: Impementor must implement SaveRow or any reordering by drag/drop won't be persisted to the database.
-    virtual void        SaveRow(int row, int newProcessOrder);
-	void                StdMenuBar(Bar &bar);
-	                    // GridCtrl is supposed to Xmlize, but I don't see it doing anything, so I've written my own.
-	                    // Have to save by name so that code changes that add/subtract columns will not cause confusion.
-	                    // Also, we save hidden state by name instead of position, which can really mess with a grid.
-	void                Xmlize(XmlIO xml);
+	virtual void          BuildBase                   (Connection * pconnection); // Call this from your implementor to do general stuff
+	virtual void          LoadBase                    (); // Call this from your implementor to do general stuff 
+	virtual void          BuildComplete               (); 
+	virtual void          LoadComplete                (); 
+	
+	virtual bool          Key                         (dword key, int count) { return GridCtrl::Key(key, count); };
+	
+	                      // Old windows have coked-up XML with bizarre widths.  This should help (menu item)
+	void                  NormalizeColumnWidth        ();
+
+	                      // Obviously, U++ developer Uno was being a total dickwad when he refused to give a function
+	                      // to extract column widths, so I've created it.
+	int                   GetFloatingColumnWidth      (int colno);
+	
+	                      // Series of corrective functions to deal with Lamo Uno's confusion about when and when not to adjust references for fixed columns
+	int                   GetFloatingColumnCount      ();
+	void                  UnhideFloatingColumnSilently(int i);
+	void                  HideFloatingColumn          (int i);
+	void                  SetFloatingColumnWidth      (int i, int w);
+	Id                    GetFloatingColumnId         (int n) const;
+	GridCtrl::ItemRect&   GetFloatingColumn           (int n);
+	int                   GetFirstSelection           ();
+	int                   CalcCorrectRow              (int row);
+	int                   GetProcessOrder             (int row = -1);
+	int                   GetMaxProcessOrder          ();
+	int                   GetNextProcessOrder         ();
+	void                  MovedRows                   ();
+	
+	                      // Trick: Impementor must implement SaveRow or any reordering by drag/drop won't be persisted to the database.
+    virtual void          SaveRow                     (int row, int newProcessOrder);
+	void                  StdMenuBar                  (Bar &bar);
+	
+	                      // GridCtrl is supposed to Xmlize, but I don't see it doing anything, so I've written my own.
+	                      // Have to save by column name so that the adding or subtracting of
+	                      // columns does not cause corruption.
+	                      // Also, we save hidden state by name instead of position, which can really mess with a grid.
+	                      // Needs work.  Gets confused around hiding/unhiding of columns with negative widths.
+	void                  Xmlize                      (XmlIO xml);
 	
 };
 
