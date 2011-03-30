4760018a8cc032e577345043e3b7d5038a5d9325
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-30 19:00:12 +0000

    - Constructed from UrpGrid - Uses shared code for Build/Load

--- trunk/src/lister/RecordViewGrid.h
+++ trunk/src/lister/RecordViewGrid.h
@@ -1,20 +1,20 @@
 #ifndef _lister_lister_RecordViewGrid_h_
 #define _lister_lister_RecordViewGrid_h_
 
-#include "ConnectedCtrl.h"
 #include <lister/Urp/UrpGrid.h>
 
 class Connection;
 
 //==============================================================================================
-class RecordViewGrid : public UrpGrid, ConnectedCtrl {
+class RecordViewGrid : public UrpGrid {
 public:
 	typedef RecordViewGrid CLASSNAME;
-                      RecordViewGrid();
-	                  // Added manually from appending a row.
-	void              Build();
-	virtual void      Load(Connection *pconnection);
-	void              Add(const String &columnName, const String &columnValue);
+	
+                          RecordViewGrid                ();
+	                      // Added manually from appending a row.
+	virtual void          Build                         (Connection *pconnection);
+	virtual void          Load                          ();
+	void                  Add                           (const String &columnName, const String &columnValue);
 };
 
 #endif
