b2cbc0e127440f76c96393e477fbcca6c200cefa
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-03-30 17:03:06 +0000

    - Added som attribuets - Prettified - Made connection a standard part of all UrpGrid classes

--- trunk/src/Urp/UrpGridCommon.h
+++ trunk/src/Urp/UrpGridCommon.h
@@ -1,15 +1,23 @@
 #ifndef _lister_Urp_UrpGridCommon_h_
 #define _lister_Urp_UrpGridCommon_h_
 
+class Connection; // Defined in lister
+
 class UrpGridCommon {
 public:
-	bool      built;
-	bool      loaded;
+	bool                  built;
+	bool                  loaded;
 	
-	UrpGridCommon() {
-		built = loaded = false;
-	}
+	// This is kept, however, it may make no sense for screens that are loaded differently,
+	// like the grids within the taskdef window.  Therefore the row is not applied and left to
+	// the code to apply where appropriate.
+	int                   lastKnownRow;
+	Connection           *connection;
 	
+	                      UrpGridCommon() {	
+	                      	built = loaded = false;	
+	                      	connection = NULL;
+	                      }
 };
 
 #endif
