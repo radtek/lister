a3ec757d43aa839f9990f8c20b5bbce572eb0758
jeffshumphreys <jeffshumphreys@97668ed5-8355-0e3d-284e-ab2976d7b0b4>
2011-05-10 23:43:03 +0000

    - Some new In functions

--- trunk/src/Urp/UrpShared.h
+++ trunk/src/Urp/UrpShared.h
@@ -26,41 +26,44 @@ namespace More {
 	bool In(String i, const char *in1, const char *in2, const char *in3);
 	bool In(String i, const char *in1, const char *in2, const char *in3, const char *in4);
 };
+
+// === Gangs of 1 ===
 template <typename T>
 bool In(T i, T in1, T in2) { if (i == in1 || i == in2) return true;	return false; }
 
+// === Gangs of 2 ===
 template <typename T, typename T2>
 bool In(T i, T2 in1, T2 in2) { if (i == in1 || i == in2) return true;	return false; }
-
 bool In(int i, int in1, int in2);
 
+// === Gangs of 3 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3) {	if (i == in1 || i == in2 || i == in3) return true; return false; }
-
 template <typename T, typename T2>
 bool In(T i, T2 in1, T2 in2, T2 in3) { if (i == in1 || i == in2 || i == in3) return true; return false; }
-
 bool In(int i, int in1, int in2, int in3);
 
+// === Gangs of 4 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4) { if (i == in1 || i == in2 || i == in3 || i == in4) return true; return false; }
-
 bool In(int i, int in1, int in2, int in3, int in4);
 
+// === Gangs of 5 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5) return true; return false; }
-
 bool In(int i, int in1, int in2, int in3, int in4, int in5);
 
+// === Gangs of 6 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5, T in6) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6) return true; return false; }
-
 bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6);
 
+// === Gangs of 7 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5, T in6, T in7) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7) return true; return false; }
 bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7);
 
+// === Gangs of 8 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5, T in6, T in7, T in8) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8) return true; return false; }
 bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8);
@@ -68,28 +71,36 @@ bool In(String i, const char *in1, const char *in2, const char *in3, const char 
 template <typename T>
 bool In(const T &i, const T &in1, const T &in2, const T &in3, const T &in4, const T &in5, const T &in6, const T &in7, const T &in8) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8) return true; return false; }
 
+// === Gangs of 9 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5, T in6, T in7, T in8, T in9) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9) return true; return false; }
 bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9);
+bool In(String i, const char *in1, const char *in2, const char *in3, const char *in4, const char *in5, const char *in6, const char *in7, const char *in8, const char *in9);
 template <typename T>
 bool In(const T &i, const T &in1, const T &in2, const T &in3, const T &in4, const T &in5, const T &in6, const T &in7, const T &in8, const T &in9) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9) return true; return false; }
 
+// === Gangs of 10 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5, T in6, T in7, T in8, T in9, T in10) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10) return true; return false; }
 bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10);
+bool In(String i, const char *in1, const char *in2, const char *in3, const char *in4, const char *in5, const char *in6, const char *in7, const char *in8, const char *in9, const char*in10);
 
+// === Gangs of 11 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5, T in6, T in7, T in8, T in9, T in10, T in11) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10 || i == in11) return true; return false; }
 bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10, int in11);
 
+// === Gangs of 12 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5, T in6, T in7, T in8, T in9, T in10, T in11, T in12) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10 || i == in11 || i == in12) return true; return false; }
 bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10, int in11, int in12);
 
+// === Gangs of 13 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5, T in6, T in7, T in8, T in9, T in10, T in11, T in12, T in13) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10 || i == in11 || i == in12 || i == in13) return true; return false; }
 bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10, int in11, int in12, int in13);
 
+// === Gangs of 14 ===
 template <typename T>
 bool In(T i, T in1, T in2, T in3, T in4, T in5, T in6, T in7, T in8, T in9, T in10, T in11, T in12, T in13, T in14) { if (i == in1 || i == in2 || i == in3 || i == in4 || i == in5 || i == in6 || i == in7 || i == in8 || i == in9 || i == in10 || i == in11 || i == in12 || i == in13 || i == in14) return true; return false; }
 bool In(int i, int in1, int in2, int in3, int in4, int in5, int in6, int in7, int in8, int in9, int in10, int in11, int in12, int in13, int in14);
